6f4b9e5465b219de4f60a5c3c60859ce4fa55bf8
Patrick Steinhardt <psteinhardt@gitlab.com>
2022-03-17 10:26:55 +0100

    git: Migrate Git exec path to use runtime directory
    Migrate the temporary Git exec path set up by the Git command factory to
    be created in the runtime directory.
    
    Changelog: changed

--- internal/git/execution_environment.go
+++ internal/git/execution_environment.go
@@ -177,7 +177,7 @@ func (c BundledGitEnvironmentConstructor) Construct(cfg config.Cfg) (_ Execution
 	// In order to support having a single Git binary only as compared to a complete Git
 	// installation, we create our own GIT_EXEC_PATH which contains symlinks to the Git
 	// binary for executables which Git expects to be present.
-	gitExecPath, err := os.MkdirTemp("", "gitaly-git-exec-path-*")
+	gitExecPath, err := os.MkdirTemp(cfg.RuntimeDir, "git-exec-*.d")
 	if err != nil {
 		return ExecutionEnvironment{}, fmt.Errorf("creating Git exec path: %w", err)
 	}
