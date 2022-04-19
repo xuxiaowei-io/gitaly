1a7197e3b2171626a95fbab2b28ecbe43ea79aea
John Cai <jcai@gitlab.com>
2022-04-19 13:40:19 -0400

    safe: Add new LockingDirectory
    Sometimes it's useful to be able to lock a directory before modifying
    it. This is similar to LockingFileWriter, except it just gives the
    ability to lock a directory so another process cannot also lock the
    directory.
    
    This will be immediately useful in RestoreCustomHooks where we are about
    ot add transactional voting to it. With transactional voting, the
    semantics are that the `Prepared` vote is made once the data that is
    about to be changed is locked.
    
    This will enable us to lock the custom hooks directory.
    
    Changelog: added

---
+++ internal/safe/locking_directory_test.go
@@ -0,0 +1,78 @@
+package safe_test
+
+import (
+	"errors"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"gitlab.com/gitlab-org/gitaly/v14/internal/safe"
+	"gitlab.com/gitlab-org/gitaly/v14/internal/testhelper"
+)
+
+func TestLockingDirectory(t *testing.T) {
+	t.Parallel()
+
+	t.Run("normal lifecycle", func(t *testing.T) {
+		path := testhelper.TempDir(t)
+		lockingDir, err := safe.NewLockingDirectory(path)
+		require.NoError(t, err)
+		require.NoError(t, lockingDir.Lock())
+		secondLockingDir, err := safe.NewLockingDirectory(path)
+		require.NoError(t, err)
+		require.NoError(t, os.WriteFile(
+			filepath.Join(path, "somefile"),
+			[]byte("data"),
+			0o644),
+		)
+		assert.ErrorIs(t, secondLockingDir.Lock(), safe.ErrFileAlreadyLocked)
+		require.NoError(t, lockingDir.Unlock())
+	})
+
+	t.Run("multiple locks fail", func(t *testing.T) {
+		path := testhelper.TempDir(t)
+		lockingDir, err := safe.NewLockingDirectory(path)
+		require.NoError(t, err)
+		require.NoError(t, lockingDir.Lock())
+		assert.Equal(
+			t,
+			errors.New("locking directory not lockable"),
+			lockingDir.Lock(),
+		)
+	})
+
+	t.Run("unlock without lock fails", func(t *testing.T) {
+		path := testhelper.TempDir(t)
+		lockingDir, err := safe.NewLockingDirectory(path)
+		require.NoError(t, err)
+		assert.Equal(
+			t,
+			errors.New("locking directory not locked"),
+			lockingDir.Unlock(),
+		)
+	})
+
+	t.Run("multiple unlocks fail", func(t *testing.T) {
+		path := testhelper.TempDir(t)
+		lockingDir, err := safe.NewLockingDirectory(path)
+		require.NoError(t, err)
+		require.NoError(t, lockingDir.Lock())
+		require.NoError(t, lockingDir.Unlock())
+		assert.Equal(
+			t,
+			errors.New("locking directory not locked"),
+			lockingDir.Unlock(),
+		)
+	})
+
+	t.Run("fails if directory is missing", func(t *testing.T) {
+		path := testhelper.TempDir(t)
+		require.NoError(t, os.RemoveAll(path))
+
+		_, err := safe.NewLockingDirectory(path)
+		assert.True(t, errors.Is(err, fs.ErrNotExist))
+	})
+}
