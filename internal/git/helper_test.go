8b41d7700fac2eeaa98e5d117276e9bcd7fa9c55
Patrick Steinhardt <psteinhardt@gitlab.com>
2022-05-03 07:49:38 +0200

    git: Rename file hosting revision verification logic
    The logic to verify that revisions are valid revisions that can safely
    be passed to various Git components is hosted in a file `proto.go`. This
    is probably a historic artifact, but it doesn't make a lot of sense
    nowadays.
    
    Rename the file to `revision.go` to make it easier to find. While at it,
    this commit also moves related tests into `revision_test.go`.

--- internal/git/helper_test.go
+++ internal/git/helper_test.go
@@ -12,30 +12,6 @@ func TestMain(m *testing.M) {
 	testhelper.Run(m)
 }
 
-func TestValidateRevision(t *testing.T) {
-	testCases := []struct {
-		rev string
-		ok  bool
-	}{
-		{rev: "foo/bar", ok: true},
-		{rev: "-foo/bar", ok: false},
-		{rev: "foo bar", ok: false},
-		{rev: "foo\x00bar", ok: false},
-		{rev: "foo/bar:baz", ok: false},
-	}
-
-	for _, tc := range testCases {
-		t.Run(tc.rev, func(t *testing.T) {
-			err := ValidateRevision([]byte(tc.rev))
-			if tc.ok {
-				require.NoError(t, err)
-			} else {
-				require.Error(t, err)
-			}
-		})
-	}
-}
-
 func newCommandFactory(tb testing.TB, cfg config.Cfg, opts ...ExecCommandFactoryOption) *ExecCommandFactory {
 	gitCmdFactory, cleanup, err := NewExecCommandFactory(cfg, opts...)
 	require.NoError(tb, err)
