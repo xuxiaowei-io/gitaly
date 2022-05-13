25d75a7c4af07ee1daea8b57ed0de798cf75f7f7
Patrick Steinhardt <psteinhardt@gitlab.com>
2022-05-13 15:31:51 +0200

    rubyserver: Ensure we sync objects if there is no Rugged gitconfig
    The Gitaly config exposes a `rugged_git_config_search_path` config that
    allows the administrator to configure where Rugged should search for the
    gitconfig it is supposed to use. While this option rather feels like an
    edge case, it is in fact currently mandatory to set this configuration
    to a gitconfig that sets `core.fsyncObjectFiles=true`. If there is no
    such configuration, then the result is that Rugged will not flush newly
    written objects to disk, which can easily lead to repository corruption.
    
    Luckily, both CNG and Omnibus know to set up this config as expected.
    But this brings its own problem with it: we have `core.fsyncObjectFiles`
    set in a central location now by distributions, which also means that
    Git as spawned by our Git command factory picks it up. But because that
    option has been deprecated in Git v2.36.0 this means that all Git
    commands would now print a warning. So we're between a rock and a hard
    place now: we must make sure that the configuration is set in Rugged,
    but it must not be present in the Git configuration.
    
    Given that this is the only configuration that Rugged really needs, and
    given that it must always be present or we otherwise corrupt repos, we
    can fix this issue by simply writing our own Gitconfig file in case
    `rugged_git_config_search_path` is unset. Like this, we can stop setting
    the search path in distributions and eventually deprecate its use.

--- internal/gitaly/rubyserver/rubyserver_test.go
+++ internal/gitaly/rubyserver/rubyserver_test.go
@@ -3,11 +3,14 @@ package rubyserver
 import (
 	"context"
 	"fmt"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"gitlab.com/gitlab-org/gitaly/v14/internal/git"
+	"gitlab.com/gitlab-org/gitaly/v14/internal/git/gittest"
 	"gitlab.com/gitlab-org/gitaly/v14/internal/gitaly/config"
 	"gitlab.com/gitlab-org/gitaly/v14/internal/gitaly/config/auth"
 	"gitlab.com/gitlab-org/gitaly/v14/internal/gitaly/config/log"
@@ -140,4 +143,83 @@ func TestSetupEnv(t *testing.T) {
 		"GIT_CONFIG_VALUE_0=false",
 		"GIT_CONFIG_COUNT=1",
 	})
+}
+
+func TestServer_gitconfig(t *testing.T) {
+	for _, tc := range []struct {
+		desc              string
+		setup             func(t *testing.T) (config.Cfg, string)
+		expectedGitconfig string
+	}{
+		{
+			desc: "writes a default gitconfig",
+			setup: func(t *testing.T) (config.Cfg, string) {
+				cfg := testcfg.Build(t)
+				expectedPath := filepath.Join(cfg.RuntimeDir, "ruby-gitconfig", "gitconfig")
+				return cfg, expectedPath
+			},
+			// If no search path is provided, we should create a placeholder file that
+			// contains all settings important to Rugged.
+			expectedGitconfig: "[core]\n\tfsyncObjectFiles = true\n",
+		},
+		{
+			desc: "uses injected gitconfig",
+			setup: func(t *testing.T) (config.Cfg, string) {
+				gitconfigDir := testhelper.TempDir(t)
+				expectedPath := filepath.Join(gitconfigDir, "gitconfig")
+				require.NoError(t, os.WriteFile(expectedPath, []byte("garbage"), 0o666))
+
+				cfg := testcfg.Build(t, testcfg.WithBase(config.Cfg{
+					Ruby: config.Ruby{
+						RuggedGitConfigSearchPath: gitconfigDir,
+					},
+				}))
+
+				return cfg, expectedPath
+			},
+			// The gitconfig shouldn't have been rewritten, so it should still contain
+			// garbage after the server starts.
+			expectedGitconfig: "garbage",
+		},
+	} {
+		t.Run(tc.desc, func(t *testing.T) {
+			ctx := testhelper.Context(t)
+			cfg, expectedPath := tc.setup(t)
+
+			gitCmdFactory := gittest.NewCommandFactory(t, cfg)
+			locator := config.NewLocator(cfg)
+
+			rubyServer := New(cfg, gitCmdFactory)
+			require.NoError(t, rubyServer.Start())
+			defer rubyServer.Stop()
+
+			// Verify that the expected gitconfig exists and has expected contents.
+			gitconfigContents := testhelper.MustReadFile(t, expectedPath)
+			require.Equal(t, tc.expectedGitconfig, string(gitconfigContents))
+
+			// Write a gitconfig that is invalidly formatted. Like this we can assert
+			// whether the Ruby server tries to read it or not because it should in fact
+			// fail.
+			require.NoError(t, os.WriteFile(expectedPath, []byte(
+				`[`,
+			), 0o666))
+
+			repo, _ := gittest.InitRepo(t, cfg, cfg.Storages[0])
+
+			// We now do any random RPC request that hits the Ruby server...
+			client, err := rubyServer.WikiServiceClient(ctx)
+			require.NoError(t, err)
+
+			ctx, err = SetHeaders(ctx, locator, repo)
+			require.NoError(t, err)
+
+			stream, err := client.WikiListPages(ctx, &gitalypb.WikiListPagesRequest{Repository: repo})
+			require.NoError(t, err)
+
+			// ... and expect it to fail with an error parsing the configuration. This
+			// demonstrates the config was injected successfully.
+			_, err = stream.Recv()
+			testhelper.RequireGrpcError(t, fmt.Errorf("Rugged::ConfigError: failed to parse config file: missing ']' in section header (in %s:1)", expectedPath), err)
+		})
+	}
 }
