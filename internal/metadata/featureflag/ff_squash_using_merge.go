c998f80533e0ec11867721d0e02bafbb9f7fcd0f
Piotr Stankowski <git@trakos.pl>
2022-05-16 17:03:03 +0200

    operations: Remove SquashUsingMerge feature flag
    Since the SquashUsingMerge feature flag has been running in
    production without issue, we can now remove it.

--- internal/metadata/featureflag/ff_squash_using_merge.go
+++
@@ -1,5 +0,0 @@
-package featureflag
-
-// SquashUsingMerge uses merge to squash commits instead of rebase and commit-tree.
-// It should be better at handling conflicts.
-var SquashUsingMerge = NewFeatureFlag("squash_using_merge", false)
