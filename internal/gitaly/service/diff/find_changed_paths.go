101e4d2c53d0233c919f560922e33b97d875a770
Kyle Edwards <kyle.edwards@kitware.com>
2022-06-02 15:45:37 -0400

    FindChangedPaths: Add requests field
    This field is more advanced, and allows you to specify either two trees
    or a commit and optional commits to treat as parents of the commit.
    This change brings FindChangedPaths in line with the full capabilities
    of git diff-tree. This also deprecates the commits field.

--- internal/gitaly/service/diff/find_changed_paths.go
+++ internal/gitaly/service/diff/find_changed_paths.go
@@ -4,11 +4,13 @@ import (
 	"bufio"
 	"bytes"
 	"context"
+	"errors"
 	"fmt"
 	"io"
 	"strings"
 
 	"gitlab.com/gitlab-org/gitaly/v15/internal/git"
+	"gitlab.com/gitlab-org/gitaly/v15/internal/git/localrepo"
 	"gitlab.com/gitlab-org/gitaly/v15/internal/helper"
 	"gitlab.com/gitlab-org/gitaly/v15/internal/helper/chunk"
 	"gitlab.com/gitlab-org/gitaly/v15/proto/go/gitalypb"
@@ -20,12 +22,35 @@ const (
 	numStatDelimiter = 0
 )
 
+// changedPathsRequestToString converts the given FindChangedPathsRequest to a string that can be passed to git-diff-tree(1). Note
+// that this function expects that all revisions have already been resolved to their respective object IDs.
+func changedPathsRequestToString(r *gitalypb.FindChangedPathsRequest_Request) (string, error) {
+	switch t := r.GetType().(type) {
+	case *gitalypb.FindChangedPathsRequest_Request_CommitRequest_:
+		return strings.Join(append([]string{t.CommitRequest.GetCommitRevision()}, t.CommitRequest.GetParentCommitRevisions()...), " "), nil
+	case *gitalypb.FindChangedPathsRequest_Request_TreeRequest_:
+		return t.TreeRequest.GetLeftTreeRevision() + " " + t.TreeRequest.GetRightTreeRevision(), nil
+	}
+
+	// This shouldn't happen
+	return "", fmt.Errorf("unknown FindChangedPathsRequest type")
+}
+
 func (s *server) FindChangedPaths(in *gitalypb.FindChangedPathsRequest, stream gitalypb.DiffService_FindChangedPathsServer) error {
 	if err := s.validateFindChangedPathsRequestParams(stream.Context(), in); err != nil {
 		return err
 	}
 
 	diffChunker := chunk.New(&findChangedPathsSender{stream: stream})
+
+	requests := make([]string, len(in.GetRequests()))
+	for i, request := range in.GetRequests() {
+		str, err := changedPathsRequestToString(request)
+		if err != nil {
+			return err
+		}
+		requests[i] = str
+	}
 
 	cmd, err := s.gitCmdFactory.New(stream.Context(), in.Repository, git.SubCmd{
 		Name: "diff-tree",
@@ -38,7 +63,7 @@ func (s *server) FindChangedPaths(in *gitalypb.FindChangedPathsRequest, stream g
 			git.Flag{Name: "--no-commit-id"},
 			git.Flag{Name: "--diff-filter=AMDTC"},
 		},
-	}, git.WithStdin(strings.NewReader(strings.Join(in.GetCommits(), "\n")+"\n")))
+	}, git.WithStdin(strings.NewReader(strings.Join(requests, "\n")+"\n")))
 	if err != nil {
 		if _, ok := status.FromError(err); ok {
 			return fmt.Errorf("stdin err: %w", err)
@@ -139,6 +164,22 @@ func (t *findChangedPathsSender) Send() error {
 	})
 }
 
+func resolveObjectWithType(ctx context.Context, repo *localrepo.Repo, revision string, expectedType string) (git.ObjectID, error) {
+	if revision == "" {
+		return "", helper.ErrInvalidArgumentf("revision cannot be empty")
+	}
+
+	oid, err := repo.ResolveRevision(ctx, git.Revision(fmt.Sprintf("%s^{%s}", revision, expectedType)))
+	if err != nil {
+		if errors.Is(err, git.ErrReferenceNotFound) {
+			return "", helper.ErrNotFoundf("revision can not be found: %q", revision)
+		}
+		return "", err
+	}
+
+	return oid, nil
+}
+
 func (s *server) validateFindChangedPathsRequestParams(ctx context.Context, in *gitalypb.FindChangedPathsRequest) error {
 	repo := in.GetRepository()
 	if _, err := s.locator.GetRepoPath(repo); err != nil {
@@ -147,18 +188,51 @@ func (s *server) validateFindChangedPathsRequestParams(ctx context.Context, in *
 
 	gitRepo := s.localrepo(in.GetRepository())
 
-	for _, commit := range in.GetCommits() {
-		if commit == "" {
-			return helper.ErrInvalidArgumentf("commits cannot contain an empty commit")
+	if len(in.GetCommits()) > 0 { //nolint:staticcheck
+		if len(in.GetRequests()) > 0 {
+			return helper.ErrInvalidArgumentf("cannot specify both commits and requests")
 		}
 
-		containsRef, err := gitRepo.HasRevision(ctx, git.Revision(commit+"^{commit}"))
-		if err != nil {
-			return fmt.Errorf("contains ref err: %w", err)
+		in.Requests = make([]*gitalypb.FindChangedPathsRequest_Request, len(in.GetCommits())) //nolint:staticcheck
+		for i, commit := range in.GetCommits() {                                              //nolint:staticcheck
+			in.Requests[i] = &gitalypb.FindChangedPathsRequest_Request{
+				Type: &gitalypb.FindChangedPathsRequest_Request_CommitRequest_{
+					CommitRequest: &gitalypb.FindChangedPathsRequest_Request_CommitRequest{
+						CommitRevision: commit,
+					},
+				},
+			}
 		}
+	}
 
-		if !containsRef {
-			return helper.ErrNotFoundf("commit: %v can not be found", commit)
+	for _, request := range in.GetRequests() {
+		switch t := request.Type.(type) {
+		case *gitalypb.FindChangedPathsRequest_Request_CommitRequest_:
+			oid, err := resolveObjectWithType(ctx, gitRepo, t.CommitRequest.GetCommitRevision(), "commit")
+			if err != nil {
+				return helper.ErrInternalf("resolving commit: %w", err)
+			}
+			t.CommitRequest.CommitRevision = oid.String()
+
+			for i, commit := range t.CommitRequest.GetParentCommitRevisions() {
+				oid, err := resolveObjectWithType(ctx, gitRepo, commit, "commit")
+				if err != nil {
+					return helper.ErrInternalf("resolving commit parent: %w", err)
+				}
+				t.CommitRequest.ParentCommitRevisions[i] = oid.String()
+			}
+		case *gitalypb.FindChangedPathsRequest_Request_TreeRequest_:
+			oid, err := resolveObjectWithType(ctx, gitRepo, t.TreeRequest.GetLeftTreeRevision(), "tree")
+			if err != nil {
+				return helper.ErrInternalf("resolving left tree: %w", err)
+			}
+			t.TreeRequest.LeftTreeRevision = oid.String()
+
+			oid, err = resolveObjectWithType(ctx, gitRepo, t.TreeRequest.GetRightTreeRevision(), "tree")
+			if err != nil {
+				return helper.ErrInternalf("resolving right tree: %w", err)
+			}
+			t.TreeRequest.RightTreeRevision = oid.String()
 		}
 	}
 
