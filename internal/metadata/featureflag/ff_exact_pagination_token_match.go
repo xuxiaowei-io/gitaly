b40f2581082a81cebbdcc96fee35f21b6805466e
Vasilii Iakliushin <viakliushin@gitlab.com>
2022-06-07 17:08:38 +0200

    Enable feature flag exact_pagination_token_match by default
    Contributes to https://gitlab.com/gitlab-org/gitaly/-/issues/3817
    
    Changelog: changed

--- internal/metadata/featureflag/ff_exact_pagination_token_match.go
+++ internal/metadata/featureflag/ff_exact_pagination_token_match.go
@@ -2,4 +2,4 @@ package featureflag
 
 // ExactPaginationTokenMatch enables exact matching for provided pagination tokens and
 // returns an error if the match is not found.
-var ExactPaginationTokenMatch = NewFeatureFlag("exact_pagination_token_match", false)
+var ExactPaginationTokenMatch = NewFeatureFlag("exact_pagination_token_match", true)
