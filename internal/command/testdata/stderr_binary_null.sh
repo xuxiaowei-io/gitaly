cc5097c9da20fc5e253255f6be53c93028f5c9e1
Patrick Steinhardt <psteinhardt@gitlab.com>
2022-06-16 14:26:29 +0200

    command: Modernize tests to follow current code style
    Modernize tests of the command package to follow our current code style.

--- internal/command/testdata/stderr_binary_null.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-dd if=/dev/zero bs=1000 count=1000 >&2
-exit 1
--- internal/command/testdata/stderr_many_lines.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-let x=0
-while [ $x -lt 100010 ]
-do
-	let x=x+1
-	printf '%06d zzzzzzzzzz\n' $x >&2
-done
-exit 1
