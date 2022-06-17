584b529003efe914d497720620e387b17be146aa
Patrick Steinhardt <psteinhardt@gitlab.com>
2022-06-17 09:48:24 +0200

    ssh: Fix racy cleanup of read monitor's pipe
    The read monitor is implemented via a Unix pipe, which means that its
    file descriptors need to always be closed by us or otherwise we have the
    potential for a resource leak. This is done by spawning a Goroutine that
    waits for the context to be cancelled. Unfortunately, when the context
    is cancelled when spawning a command that is about to use these file
    descriptors then we now run into a race: `exec.Cmd.Start()` may just be
    about to duplicate those file descriptors to pass them to the child
    process while we're in the process of closing them.
    
    Fix this race by cleaning them up inside of the command's finalizer
    instead. Like this we ensure that the descriptors are only closed after
    we have already reaped the child process.
    
    Changelog: fixed

--- internal/gitaly/service/ssh/monitor_stdin_command.go
+++ internal/gitaly/service/ssh/monitor_stdin_command.go
@@ -11,16 +11,20 @@ import (
 )
 
 func monitorStdinCommand(ctx context.Context, gitCmdFactory git.CommandFactory, stdin io.Reader, stdout, stderr io.Writer, sc git.SubCmd, opts ...git.CmdOpt) (*command.Command, *pktline.ReadMonitor, error) {
-	stdinPipe, monitor, err := pktline.NewReadMonitor(ctx, stdin)
+	stdinPipe, monitor, cleanup, err := pktline.NewReadMonitor(ctx, stdin)
 	if err != nil {
 		return nil, nil, fmt.Errorf("create monitor: %v", err)
 	}
 
 	cmd, err := gitCmdFactory.NewWithoutRepo(ctx, sc, append([]git.CmdOpt{
-		git.WithStdin(stdinPipe), git.WithStdout(stdout), git.WithStderr(stderr),
+		git.WithStdin(stdinPipe),
+		git.WithStdout(stdout),
+		git.WithStderr(stderr),
+		git.WithFinalizer(func(*command.Command) { cleanup() }),
 	}, opts...)...)
 	stdinPipe.Close() // this now belongs to cmd
 	if err != nil {
+		cleanup()
 		return nil, nil, fmt.Errorf("start cmd: %v", err)
 	}
 
