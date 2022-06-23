6af8bfd75320ea4c6408fa557a717c16493e4ad8
Patrick Steinhardt <psteinhardt@gitlab.com>
2022-06-23 08:50:28 +0200

    diff: Convert `RawDiff()` test to not use worktree
    The use of worktrees in our tests is discouraged in favor of using test
    helpers like `git.WriteCommit()` to set up required test data. One of
    our tests for `RawDiff()` is using a worktree though to verify that the
    generated diff can in fact be reapplied to a repository to round-trip to
    the same tree again, and git-apply(1) requires a worktree to apply
    patches.
    
    Convert the test by instead using `gitaly-git2go apply` to apply the
    patch. Note that we're forced to use a different set of commits to do
    this test though: the current difference contains changes to binary
    files, and these cannot be handled by `gitaly-git2go`.

--- internal/gitaly/service/diff/raw_test.go
+++ internal/gitaly/service/diff/raw_test.go
@@ -1,14 +1,17 @@
 package diff
 
 import (
-	"fmt"
 	"io"
 	"regexp"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 	"gitlab.com/gitlab-org/gitaly/v15/internal/git/gittest"
+	"gitlab.com/gitlab-org/gitaly/v15/internal/git2go"
+	"gitlab.com/gitlab-org/gitaly/v15/internal/gitaly/config"
 	"gitlab.com/gitlab-org/gitaly/v15/internal/testhelper"
+	"gitlab.com/gitlab-org/gitaly/v15/internal/testhelper/testcfg"
 	"gitlab.com/gitlab-org/gitaly/v15/proto/go/gitalypb"
 	"gitlab.com/gitlab-org/gitaly/v15/streamio"
 	"google.golang.org/grpc/codes"
@@ -18,38 +21,55 @@ func TestRawDiff_successful(t *testing.T) {
 	t.Parallel()
 
 	ctx := testhelper.Context(t)
-	cfg, repo, repoPath, client := setupDiffService(ctx, t)
+	cfg, repoProto, repoPath, client := setupDiffService(ctx, t)
+	testcfg.BuildGitalyGit2Go(t, cfg)
 
+	gitCmdFactory := gittest.NewCommandFactory(t, cfg)
+	locator := config.NewLocator(cfg)
+	git2goExecutor := git2go.NewExecutor(cfg, gitCmdFactory, locator)
+
+	leftCommit := "57290e673a4c87f51294f5216672cbc58d485d25"
 	rightCommit := "e395f646b1499e8e0279445fc99a0596a65fab7e"
-	leftCommit := "8a0f2ee90d940bfb0ba1e14e8214b0649056e4ab"
-	rpcRequest := &gitalypb.RawDiffRequest{Repository: repo, RightCommitId: rightCommit, LeftCommitId: leftCommit}
 
-	c, err := client.RawDiff(ctx, rpcRequest)
+	stream, err := client.RawDiff(ctx, &gitalypb.RawDiffRequest{
+		Repository:    repoProto,
+		LeftCommitId:  leftCommit,
+		RightCommitId: rightCommit,
+	})
 	require.NoError(t, err)
 
-	_, sandboxRepoPath := gittest.CloneRepo(t, cfg, cfg.Storages[0], gittest.CloneRepoOpts{
-		WithWorktree: true,
-	})
-
-	reader := streamio.NewReader(func() ([]byte, error) {
-		response, err := c.Recv()
+	rawDiff, err := io.ReadAll(streamio.NewReader(func() ([]byte, error) {
+		response, err := stream.Recv()
 		return response.GetData(), err
+	}))
+	require.NoError(t, err)
+
+	signature := git2go.Signature{
+		Name:  "Scrooge McDuck",
+		Email: "scrooge@mcduck.com",
+		When:  time.Unix(12345, 0),
+	}
+
+	// Now that we have read the patch in we verify that it indeed round-trips to the same tree
+	// as the right commit is referring to by reapplying the diff on top of the left commit.
+	patchedCommitID, err := git2goExecutor.Apply(ctx, gittest.RewrittenRepository(ctx, t, cfg, repoProto), git2go.ApplyParams{
+		Repository:   repoPath,
+		Committer:    signature,
+		ParentCommit: leftCommit,
+		Patches: git2go.NewSlicePatchIterator([]git2go.Patch{{
+			Author:  signature,
+			Message: "Applying received raw diff",
+			Diff:    rawDiff,
+		}}),
 	})
+	require.NoError(t, err)
 
-	committerName := "Scrooge McDuck"
-	committerEmail := "scrooge@mcduck.com"
-	gittest.Exec(t, cfg, "-C", sandboxRepoPath, "reset", "--hard", leftCommit)
-
-	gittest.ExecOpts(t, cfg, gittest.ExecConfig{Stdin: reader}, "-C", sandboxRepoPath, "apply")
-	gittest.Exec(t, cfg, "-C", sandboxRepoPath, "add", ".")
-	gittest.Exec(t, cfg, "-C", sandboxRepoPath,
-		"-c", fmt.Sprintf("user.name=%s", committerName),
-		"-c", fmt.Sprintf("user.email=%s", committerEmail),
-		"commit", "-m", "Applying received raw diff")
-
-	expectedTreeStructure := gittest.Exec(t, cfg, "-C", repoPath, "ls-tree", "-r", rightCommit)
-	actualTreeStructure := gittest.Exec(t, cfg, "-C", sandboxRepoPath, "ls-tree", "-r", "HEAD")
-	require.Equal(t, expectedTreeStructure, actualTreeStructure)
+	// Peel both right commit and patched commit to their trees and assert that they refer to
+	// the same one.
+	require.Equal(t,
+		gittest.Exec(t, cfg, "-C", repoPath, "rev-parse", rightCommit+"^{tree}"),
+		gittest.Exec(t, cfg, "-C", repoPath, "rev-parse", patchedCommitID.String()+"^{tree}"),
+	)
 }
 
 func TestRawDiff_inputValidation(t *testing.T) {
