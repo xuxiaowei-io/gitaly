712b2ff9b2e8c20726344b479f778c3cac29b45b
Patrick Steinhardt <psteinhardt@gitlab.com>
2022-06-27 10:39:03 +0200

    gittest: Add helper to resolve revisions to an object ID
    Add helper function to resolve revisions to an object ID, which is a
    frequently used pattern in our tests.

---
+++ internal/git/gittest/ref_test.go
@@ -0,0 +1,17 @@
+package gittest
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"gitlab.com/gitlab-org/gitaly/v15/internal/git"
+)
+
+func TestResolveRevision(t *testing.T) {
+	cfg, _, repoPath := setup(t)
+
+	require.Equal(t,
+		git.ObjectID("1e292f8fedd741b75372e19097c76d327140c312"),
+		ResolveRevision(t, cfg, repoPath, "refs/heads/master"),
+	)
+}
