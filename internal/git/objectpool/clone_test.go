b4e9c00b6b9e64b38d79ce33258ed03abb7945b6
Patrick Steinhardt <psteinhardt@gitlab.com>
2022-07-05 13:08:04 +0200

    objectpool: Improve clone tests to match modern best practices
    Improve the clone tests to match modern best practices. This adds
    missing `t.Parallel()` calls, adjusts test names and removes useless
    calls to remove pools after the test. Last but not least this also
    tightens error checks to assert we fail in an expected way.

--- internal/git/objectpool/clone_test.go
+++ internal/git/objectpool/clone_test.go
@@ -1,6 +1,7 @@
 package objectpool
 
 import (
+	"fmt"
 	"path/filepath"
 	"testing"
 
@@ -9,32 +10,35 @@ import (
 	"gitlab.com/gitlab-org/gitaly/v15/internal/testhelper"
 )
 
-func TestClone(t *testing.T) {
+func TestClone_successful(t *testing.T) {
+	t.Parallel()
+
 	ctx := testhelper.Context(t)
 
 	cfg, pool, repoProto := setupObjectPool(t, ctx)
 	repo := localrepo.NewTestRepo(t, cfg, repoProto)
 
 	require.NoError(t, pool.clone(ctx, repo))
-	defer func() {
-		require.NoError(t, pool.Remove(ctx))
-	}()
 
 	require.DirExists(t, pool.FullPath())
 	require.DirExists(t, filepath.Join(pool.FullPath(), "objects"))
 }
 
-func TestCloneExistingPool(t *testing.T) {
+func TestClone_existingPool(t *testing.T) {
+	t.Parallel()
+
 	ctx := testhelper.Context(t)
 
 	cfg, pool, repoProto := setupObjectPool(t, ctx)
 	repo := localrepo.NewTestRepo(t, cfg, repoProto)
 
+	// The first time around cloning should succeed, but ...
 	require.NoError(t, pool.clone(ctx, repo))
-	defer func() {
-		require.NoError(t, pool.Remove(ctx))
-	}()
 
-	// re-clone on the directory
-	require.Error(t, pool.clone(ctx, repo))
+	// ... when we try to clone the same pool a second time we should get an error because the
+	// destination exists already.
+	require.EqualError(t, pool.clone(ctx, repo), fmt.Sprintf(
+		"cloning to pool: exit status 128, stderr: \"fatal: destination path '%s' already exists and is not an empty directory.\\n\"",
+		pool.FullPath(),
+	))
 }
