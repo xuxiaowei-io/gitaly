7921a13d32e8b4860727d87c0237eb9ea2dfc4fe
Patrick Steinhardt <psteinhardt@gitlab.com>
2022-07-07 12:48:19 +0200

    helper: Convert gRPC `Errorf()` tests to use more subtests
    The tests for `Errorf()`-style gRPC error wrapping check various
    different cases, but do so in-line without the use of subtests. This
    makes it hard to grasp the scope and that we are testing different
    functionality.
    
    Use subtests to clearly highlight the scope.

--- internal/helper/error_test.go
+++ internal/helper/error_test.go
@@ -147,35 +147,43 @@ func testErrorfFormat(t *testing.T, errorFormat, errorFormatEqual string) {
 		t.Run(tc.desc, func(t *testing.T) {
 			require.NotEqual(t, tc.code, inputGRPCCode, "canary test code and tc.code may not be the same")
 
-			// When not re-throwing an error we get the GRPC error code corresponding to
-			// the function's name. Just like the non-f functions.
-			err := tc.errorf(errorFormat, input)
-			require.EqualError(t, err, fmt.Sprintf(errorFormatEqual, errorMessage))
-			require.False(t, errors.Is(err, inputGRPC))
-			require.Equal(t, tc.code, status.Code(err))
-
-			// When wrapping an existing gRPC error, then the error code will stay the
-			// same.
-			err = tc.errorf(errorFormat, inputGRPCFmt)
-			require.False(t, errors.Is(err, input))
-			if isFormatW {
-				require.Equal(t, inputGRPCCode, status.Code(err))
-			} else {
+			t.Run("with non-gRPC error", func(t *testing.T) {
+				// When not re-throwing an error we get the GRPC error code
+				// corresponding to the function's name. Just like the non-f
+				// functions.
+				err := tc.errorf(errorFormat, input)
+				require.EqualError(t, err, fmt.Sprintf(errorFormatEqual, errorMessage))
+				require.False(t, errors.Is(err, inputGRPC))
 				require.Equal(t, tc.code, status.Code(err))
-			}
-			require.NotEqual(t, tc.code, status.Code(inputGRPC))
+			})
 
-			// The same as above, except that we test with an error returned by
-			// `status.Error()`.
-			errX := tc.errorf(errorFormat, inputGRPC)
-			require.Equal(t, errors.Is(errX, inputGRPC), isFormatW) // .True() for non-f
-			require.False(t, errors.Is(errX, input))
-			if isFormatW {
-				require.Equal(t, inputGRPCCode, status.Code(errX))
-			} else {
-				require.Equal(t, tc.code, status.Code(errX))
-			}
-			require.Equal(t, inputGRPCCode, status.Code(inputGRPC))
+			t.Run("with status.Errorf error", func(t *testing.T) {
+				// When wrapping an existing gRPC error, then the error code will
+				// stay the same.
+
+				err := tc.errorf(errorFormat, inputGRPCFmt)
+				require.False(t, errors.Is(err, input))
+				if isFormatW {
+					require.Equal(t, inputGRPCCode, status.Code(err))
+				} else {
+					require.Equal(t, tc.code, status.Code(err))
+				}
+				require.NotEqual(t, tc.code, status.Code(inputGRPC))
+			})
+
+			t.Run("with status.Error error", func(t *testing.T) {
+				// The same as above, except that we test with an error returned by
+				// `status.Error()`.
+				err := tc.errorf(errorFormat, inputGRPC)
+				require.Equal(t, errors.Is(err, inputGRPC), isFormatW) // .True() for non-f
+				require.False(t, errors.Is(err, input))
+				if isFormatW {
+					require.Equal(t, inputGRPCCode, status.Code(err))
+				} else {
+					require.Equal(t, tc.code, status.Code(err))
+				}
+				require.Equal(t, inputGRPCCode, status.Code(inputGRPC))
+			})
 		})
 	}
 }
