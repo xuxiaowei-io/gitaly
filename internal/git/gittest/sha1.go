65a8b26c762023c57ae70221c4811bcaa5569765
Patrick Steinhardt <psteinhardt@gitlab.com>
2022-07-18 12:49:20 +0200

    gittest: Introduce default object hash
    The gittest package is currently re-exporting the functionality provided
    by the `ObjectHash` structure. Depending on whether we are testing with
    SHA1 or SHA256, we export either the implementation details of the
    `ObjectHashSHA1` or `ObjectHashSHA256` structure.
    
    Now that we have the hash-specific information neatly encapsulated in
    its own structure there is no need to re-export the separate details of
    that structure anymore. Instead, refactor the gittest package to only
    export a single `DefaultObjectHash` variable that points to one of the
    above structures, depending on what we're currently testing.

--- internal/git/gittest/sha1.go
+++ internal/git/gittest/sha1.go
@@ -4,14 +4,9 @@ package gittest
 
 import "gitlab.com/gitlab-org/gitaly/v15/internal/git"
 
-// EmptyTreeOID is the Git tree object hash that corresponds to an empty tree (directory)
-var EmptyTreeOID = git.ObjectHashSHA1.EmptyTreeOID
+var (
+	// DefaultObjectHash is the default hash used for running tests.
+	DefaultObjectHash = git.ObjectHashSHA1
 
-var initRepoExtraArgs = []string{}
-
-// NewObjectIDFromHex constructs a new ObjectID from the given hex
-// representation of the object ID. Returns ErrInvalidObjectID if the given
-// OID is not valid.
-func NewObjectIDFromHex(hex string) (git.ObjectID, error) {
-	return git.ObjectHashSHA1.FromHex(hex)
-}
+	initRepoExtraArgs = []string{}
+)
--- internal/git/gittest/sha256.go
+++ internal/git/gittest/sha256.go
@@ -4,19 +4,11 @@ package gittest
 
 import (
 	"gitlab.com/gitlab-org/gitaly/v15/internal/git"
-	"gitlab.com/gitlab-org/gitaly/v15/internal/git/sha256"
 )
 
-const (
-	// EmptyTreeOID is the Git tree object hash that corresponds to an empty tree (directory)
-	EmptyTreeOID = sha256.EmptyTreeOID
+var (
+	// DefaultObjectHash is the default hash used for running tests.
+	DefaultObjectHash = git.ObjectHashSHA256
+
+	initRepoExtraArgs = []string{"--object-format=sha256"}
 )
-
-var initRepoExtraArgs = []string{"--object-format=sha256"}
-
-// NewObjectIDFromHex constructs a new ObjectID from the given hex
-// representation of the object ID. Returns ErrInvalidObjectID if the given
-// OID is not valid.
-func NewObjectIDFromHex(hex string) (git.ObjectID, error) {
-	return sha256.NewObjectIDFromHex(hex)
-}
