63f16bfe67f7ba561652722e90793b219a2b3960
John Cai <jcai@gitlab.com>
2022-08-08 15:16:57 -0400

    cgroups: Add PruneOldCgroups helper
    Add a helper function to specifically clean out old cgroups from both
    the memory and cpu subsystems.

--- internal/cgroups/cgroups.go
+++ internal/cgroups/cgroups.go
@@ -1,9 +1,13 @@
 package cgroups
 
 import (
+	"path/filepath"
+
 	"github.com/prometheus/client_golang/prometheus"
+	log "github.com/sirupsen/logrus"
 	"gitlab.com/gitlab-org/gitaly/v15/internal/command"
 	"gitlab.com/gitlab-org/gitaly/v15/internal/git/repository"
+	"gitlab.com/gitlab-org/gitaly/v15/internal/gitaly/config"
 	"gitlab.com/gitlab-org/gitaly/v15/internal/gitaly/config/cgroups"
 )
 
@@ -30,4 +34,27 @@ func NewManager(cfg cgroups.Config, pid int) Manager {
 	}
 
 	return &NoopManager{}
+}
+
+// PruneOldCgroups prunes old cgroups for both the memory and cpu subsystems
+func PruneOldCgroups(cfg cgroups.Config, logger log.FieldLogger) {
+	if cfg.HierarchyRoot == "" {
+		return
+	}
+
+	if err := config.PruneOldGitalyProcessDirectories(
+		logger,
+		filepath.Join(cfg.Mountpoint, "memory",
+			cfg.HierarchyRoot),
+	); err != nil {
+		logger.WithError(err).Error("failed to clean up memory cgroups")
+	}
+
+	if err := config.PruneOldGitalyProcessDirectories(
+		logger,
+		filepath.Join(cfg.Mountpoint, "cpu",
+			cfg.HierarchyRoot),
+	); err != nil {
+		logger.WithError(err).Error("failed to clean up cpu cgroups")
+	}
 }
