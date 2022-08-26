85df07cf1f7ccd84f52d308ece166afa2271cb89
Karthik Nayak <knayak@gitlab.com>
2022-08-26 10:58:32 +0200

    featureflag: Add new flag 'SimplifyFindLocalBranchesResponse'
    Add a new flag 'SimplifyFindLocalBranchesResponse' which will facilitate
    the addition of the new response type to 'FindLocalBranchesResponse'.
    The new field 'LocalBranches' is meant to deprecate the existing
    'Branches' field. The new field is of the generic 'Branch' type.

---
+++ internal/metadata/featureflag/ff_simplify_find_local_branches_response.go
@@ -0,0 +1,10 @@
+package featureflag
+
+// SimplifyFindLocalBranchesResponse enables the simplification of FindLocalBranchesRespnose
+// by adding the generic Branch type in the response
+var SimplifyFindLocalBranchesResponse = NewFeatureFlag(
+	"simplify_find_local_branches_response",
+	"v15.4.0",
+	"https://gitlab.com/gitlab-org/gitaly/-/issues/1294",
+	false,
+)
