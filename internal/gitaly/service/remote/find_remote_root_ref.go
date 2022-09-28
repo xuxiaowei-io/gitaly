8e8d639d420088b9cd0ee8103d487e0952472f53
Karthik Nayak <knayak@gitlab.com>
2022-09-28 21:45:38 +0200

    remote: Use `GetURLAndResolveConfig()`
    Use the `GetURLAndResolveConfig()` to get modified URL and any
    configPair if necessary to avoid DNS rebinding. Use this modified URL
    instead and add the configPair to the git subcommand.
    
    To make it easier to test, move the command instantiation part to a new
    separate function `findRemoteRootRefCmd`. This makes it easier to test
    the changes to the `git.Cmd`.

--- internal/gitaly/service/remote/find_remote_root_ref.go
+++ internal/gitaly/service/remote/find_remote_root_ref.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"strings"
 
+	"gitlab.com/gitlab-org/gitaly/v15/internal/command"
 	gitalyerrors "gitlab.com/gitlab-org/gitaly/v15/internal/errors"
 	"gitlab.com/gitlab-org/gitaly/v15/internal/git"
 	"gitlab.com/gitlab-org/gitaly/v15/internal/helper"
@@ -14,10 +15,21 @@ import (
 
 const headPrefix = "HEAD branch: "
 
-func (s *server) findRemoteRootRef(ctx context.Context, request *gitalypb.FindRemoteRootRefRequest) (string, error) {
-	config := []git.ConfigPair{
-		{Key: "remote.inmemory.url", Value: request.RemoteUrl},
+func (s *server) findRemoteRootRefCmd(ctx context.Context, request *gitalypb.FindRemoteRootRefRequest) (*command.Command, error) {
+	remoteURL := request.GetRemoteUrl()
+	var config []git.ConfigPair
+
+	if resolvedAddress := request.GetResolvedAddress(); resolvedAddress != "" {
+		modifiedURL, resolveConfig, err := git.GetURLAndResolveConfig(remoteURL, resolvedAddress)
+		if err != nil {
+			return nil, helper.ErrInvalidArgumentf("couldn't get curloptResolve config: %w", err)
+		}
+
+		remoteURL = modifiedURL
+		config = append(config, resolveConfig...)
 	}
+
+	config = append(config, git.ConfigPair{Key: "remote.inmemory.url", Value: remoteURL})
 
 	if authHeader := request.GetHttpAuthorizationHeader(); authHeader != "" {
 		config = append(config, git.ConfigPair{
@@ -33,7 +45,7 @@ func (s *server) findRemoteRootRef(ctx context.Context, request *gitalypb.FindRe
 		})
 	}
 
-	cmd, err := s.gitCmdFactory.New(ctx, request.Repository,
+	return s.gitCmdFactory.New(ctx, request.Repository,
 		git.SubSubCmd{
 			Name:   "remote",
 			Action: "show",
@@ -42,6 +54,10 @@ func (s *server) findRemoteRootRef(ctx context.Context, request *gitalypb.FindRe
 		git.WithRefTxHook(request.Repository),
 		git.WithConfigEnv(config...),
 	)
+}
+
+func (s *server) findRemoteRootRef(ctx context.Context, request *gitalypb.FindRemoteRootRefRequest) (string, error) {
+	cmd, err := s.findRemoteRootRefCmd(ctx, request)
 	if err != nil {
 		return "", err
 	}
