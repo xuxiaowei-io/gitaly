c4266e931b056c86f5e0af8e31646fb0357747c9
John Cai <jcai@gitlab.com>
2022-10-10 11:22:12 -0400

    Revert "Merge branch 'revert-f955266a' into 'master'"
    This reverts commit 847af5af1cb914da74f3f9c5f3e774237ab10e14, reversing
    changes made to f955266a86bb86637389583c591ae79ed7ad460e.

--- internal/git/command_factory.go
+++ internal/git/command_factory.go
@@ -43,6 +43,7 @@ type CommandFactory interface {
 type execCommandFactoryConfig struct {
 	hooksPath     string
 	gitBinaryPath string
+	gitalyPid     int
 }
 
 // ExecCommandFactoryOption is an option that can be passed to NewExecCommandFactory.
@@ -69,6 +70,14 @@ func WithGitBinaryPath(path string) ExecCommandFactoryOption {
 	}
 }
 
+// WithGitalyPid will cause new commands to be started under the gitaly pid as
+// the pgid
+func WithGitalyPid(gitalyPid int) ExecCommandFactoryOption {
+	return func(cfg *execCommandFactoryConfig) {
+		cfg.gitalyPid = gitalyPid
+	}
+}
+
 type hookDirectories struct {
 	tempHooksPath string
 }
@@ -89,6 +98,8 @@ type ExecCommandFactory struct {
 
 	cachedGitVersionLock     sync.RWMutex
 	cachedGitVersionByBinary map[string]cachedGitVersion
+
+	gitalyPid int
 }
 
 // NewExecCommandFactory returns a new instance of initialized ExecCommandFactory. The returned
@@ -138,6 +149,7 @@ func NewExecCommandFactory(cfg config.Cfg, opts ...ExecCommandFactoryOption) (_ 
 		),
 		hookDirs:                 hookDirectories,
 		cachedGitVersionByBinary: make(map[string]cachedGitVersion),
+		gitalyPid:                factoryCfg.gitalyPid,
 	}
 
 	return gitCmdFactory, runCleanups, nil
@@ -413,6 +425,7 @@ func (cf *ExecCommandFactory) newCommand(ctx context.Context, repo repository.Gi
 		command.WithCommandName("git", sc.Subcommand()),
 		command.WithCgroup(cf.cgroupsManager, repo),
 		command.WithCommandGitVersion(cmdGitVersion.String()),
+		command.WithParent(cf.gitalyPid),
 	)...)
 	if err != nil {
 		return nil, err
