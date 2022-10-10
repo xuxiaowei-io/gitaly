xuxiaowei-io/gitaly

commit 80d62284b148521cd0377b40aaae4aee81f0db62

ref: Return structured error when update error is not ErrAlreadyLocked
We have a bug wherein we still return a successful response with an
error embedded in the response if the Prepare() step fails with an error
but the error is not an ErrAlreadyLocked. Fix the code such that we
return an error instead of a response when there is a non
ErrAlreadyLocked error during the Prepare() call.

Changelog: fixed

internal/gitaly/service/ref/delete_refs.go
@@ -83,24 +83,27 @@ func (s *server) DeleteRefs(ctx context.Context, in *gitalypb.DeleteRefsRequest)
 	}
 
 	if err := updater.Prepare(); err != nil {
-		var errAlreadyLocked *updateref.ErrAlreadyLocked
-		if featureflag.DeleteRefsStructuredErrors.IsEnabled(ctx) &&
-			errors.As(err, &errAlreadyLocked) {
-			detailedErr, err := helper.ErrWithDetails(
-				helper.ErrFailedPreconditionf("cannot lock references"),
-				&gitalypb.DeleteRefsError{
-					Error: &gitalypb.DeleteRefsError_ReferencesLocked{
-						ReferencesLocked: &gitalypb.ReferencesLockedError{
-							Refs: [][]byte{[]byte(errAlreadyLocked.Ref)},
+		if featureflag.DeleteRefsStructuredErrors.IsEnabled(ctx) {
+			var errAlreadyLocked *updateref.ErrAlreadyLocked
+			if errors.As(err, &errAlreadyLocked) {
+				detailedErr, err := helper.ErrWithDetails(
+					helper.ErrFailedPreconditionf("cannot lock references"),
+					&gitalypb.DeleteRefsError{
+						Error: &gitalypb.DeleteRefsError_ReferencesLocked{
+							ReferencesLocked: &gitalypb.ReferencesLockedError{
+								Refs: [][]byte{[]byte(errAlreadyLocked.Ref)},
+							},
 						},
 					},
-				},
-			)
-			if err != nil {
-				return nil, helper.ErrInternalf("error details: %w", err)
+				)
+				if err != nil {
+					return nil, helper.ErrInternalf("error details: %w", err)
+				}
+
+				return nil, detailedErr
 			}
 
-			return nil, detailedErr
+			return nil, helper.ErrInternalf("unable to prepare: %w", err)
 		}
 
 		return &gitalypb.DeleteRefsResponse{

commit d5e420b6c6c918b3607169c09f6cd0d9f4e2486f

ref: Revert fix for DeleteRefs that broke QA
With 80d62284b (ref: Return structured error when update error is not
ErrAlreadyLocked, 2022-09-28) we have fixed a case where we still
returned successfully on error even though the feature flag to use
structured errors was enabled. This fix caused breakage in our QA jobs
though:

    1st Try error in ./spec/lib/gitlab/git/repository_spec.rb:463:
    expected Gitlab::Git::Repository::GitError, got #<Gitlab::Git::CommandError: 13:unable to prepare: state update to "prepare" failed: EOF, stderr: "fatal: invalid ref format: refs\\heads\\fix\n".> with backtrace:
    downstream QA

Revert this change for now to unblock deployments.

internal/gitaly/service/ref/delete_refs.go
@@ -83,27 +83,24 @@ func (s *server) DeleteRefs(ctx context.Context, in *gitalypb.DeleteRefsRequest)
 	}
 
 	if err := updater.Prepare(); err != nil {
-		if featureflag.DeleteRefsStructuredErrors.IsEnabled(ctx) {
-			var errAlreadyLocked *updateref.ErrAlreadyLocked
-			if errors.As(err, &errAlreadyLocked) {
-				detailedErr, err := helper.ErrWithDetails(
-					helper.ErrFailedPreconditionf("cannot lock references"),
-					&gitalypb.DeleteRefsError{
-						Error: &gitalypb.DeleteRefsError_ReferencesLocked{
-							ReferencesLocked: &gitalypb.ReferencesLockedError{
-								Refs: [][]byte{[]byte(errAlreadyLocked.Ref)},
-							},
+		var errAlreadyLocked *updateref.ErrAlreadyLocked
+		if featureflag.DeleteRefsStructuredErrors.IsEnabled(ctx) &&
+			errors.As(err, &errAlreadyLocked) {
+			detailedErr, err := helper.ErrWithDetails(
+				helper.ErrFailedPreconditionf("cannot lock references"),
+				&gitalypb.DeleteRefsError{
+					Error: &gitalypb.DeleteRefsError_ReferencesLocked{
+						ReferencesLocked: &gitalypb.ReferencesLockedError{
+							Refs: [][]byte{[]byte(errAlreadyLocked.Ref)},
 						},
 					},
-				)
-				if err != nil {
-					return nil, helper.ErrInternalf("error details: %w", err)
-				}
-
-				return nil, detailedErr
+				},
+			)
+			if err != nil {
+				return nil, helper.ErrInternalf("error details: %w", err)
 			}
 
-			return nil, helper.ErrInternalf("unable to prepare: %w", err)
+			return nil, detailedErr
 		}
 
 		return &gitalypb.DeleteRefsResponse{