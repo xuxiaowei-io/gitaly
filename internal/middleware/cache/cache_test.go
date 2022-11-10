d7b1de613c402122022f50fa53434683d0a60aac
Patrick Steinhardt <psteinhardt@gitlab.com>
2022-11-10 09:04:01 +0100

    cache: Rewrite tests to be table-driven
    The tests for our cache middleware are really hard to understand as a
    single testcase is used to verify behaviour with different kinds of gRPC
    requests. Refactor them to instead be table-driven.

--- internal/middleware/cache/cache_test.go
+++ internal/middleware/cache/cache_test.go
@@ -9,7 +9,6 @@ import (
 	"sync"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	diskcache "gitlab.com/gitlab-org/gitaly/v15/internal/cache"
 	"gitlab.com/gitlab-org/gitaly/v15/internal/middleware/cache/testdata"
@@ -17,36 +16,16 @@ import (
 	"gitlab.com/gitlab-org/gitaly/v15/internal/testhelper"
 	"gitlab.com/gitlab-org/gitaly/v15/proto/go/gitalypb"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials/insecure"
-	"google.golang.org/grpc/health"
 	"google.golang.org/grpc/health/grpc_health_v1"
-	"google.golang.org/grpc/status"
 )
 
-//go:generate make testdata/stream.pb.go
 func TestInvalidators(t *testing.T) {
-	mCache := newMockCache()
+	t.Parallel()
 
-	reg, err := protoregistry.NewFromPaths("middleware/cache/testdata/stream.proto")
-	require.NoError(t, err)
-
-	srvr := grpc.NewServer(
-		grpc.StreamInterceptor(
-			StreamInvalidator(mCache, reg),
-		),
-		grpc.UnaryInterceptor(
-			UnaryInvalidator(mCache, reg),
-		),
-	)
 	ctx := testhelper.Context(t)
 
-	svc := &testSvc{}
-
-	cli, cc, cleanup := newTestSvc(t, ctx, srvr, svc)
-	defer cleanup()
-
-	repo1 := &gitalypb.Repository{
+	repo := &gitalypb.Repository{
 		GitAlternateObjectDirectories: []string{"1"},
 		GitObjectDirectory:            "1",
 		GlProjectPath:                 "1",
@@ -55,90 +34,137 @@ func TestInvalidators(t *testing.T) {
 		StorageName:                   "1",
 	}
 
-	repo2 := &gitalypb.Repository{
-		GitAlternateObjectDirectories: []string{"2"},
-		GitObjectDirectory:            "2",
-		GlProjectPath:                 "2",
-		GlRepository:                  "2",
-		RelativePath:                  "2",
-		StorageName:                   "2",
+	for _, tc := range []struct {
+		desc                  string
+		invokeRPC             func(*testing.T, *grpc.ClientConn)
+		expectedInvalidations []*gitalypb.Repository
+	}{
+		{
+			desc: "streaming accessor does not invalidate cache",
+			invokeRPC: func(t *testing.T, conn *grpc.ClientConn) {
+				stream, err := testdata.NewTestServiceClient(conn).ClientStreamRepoAccessor(ctx, &testdata.Request{
+					Destination: repo,
+				})
+				require.NoError(t, err)
+
+				_, err = stream.Recv()
+				require.Equal(t, err, io.EOF)
+			},
+		},
+		{
+			desc: "streaming maintainer does not invalidate cache",
+			invokeRPC: func(t *testing.T, conn *grpc.ClientConn) {
+				stream, err := testdata.NewTestServiceClient(conn).ClientStreamRepoMaintainer(ctx, &testdata.Request{
+					Destination: repo,
+				})
+				require.NoError(t, err)
+
+				_, err = stream.Recv()
+				require.Equal(t, err, io.EOF)
+			},
+		},
+		{
+			desc: "streaming mutator invalidates cache",
+			invokeRPC: func(t *testing.T, conn *grpc.ClientConn) {
+				stream, err := testdata.NewTestServiceClient(conn).ClientStreamRepoMutator(ctx, &testdata.Request{
+					Destination: repo,
+				})
+				require.NoError(t, err)
+
+				_, err = stream.Recv()
+				require.Equal(t, err, io.EOF)
+			},
+			expectedInvalidations: []*gitalypb.Repository{repo},
+		},
+		{
+			desc: "unary accessor does not invalidate cache",
+			invokeRPC: func(t *testing.T, conn *grpc.ClientConn) {
+				_, err := testdata.NewTestServiceClient(conn).ClientUnaryRepoAccessor(ctx, &testdata.Request{
+					Destination: repo,
+				})
+				require.NoError(t, err)
+			},
+		},
+		{
+			desc: "unary maintainer does not invalidate cache",
+			invokeRPC: func(t *testing.T, conn *grpc.ClientConn) {
+				_, err := testdata.NewTestServiceClient(conn).ClientUnaryRepoMaintainer(ctx, &testdata.Request{
+					Destination: repo,
+				})
+				require.NoError(t, err)
+			},
+		},
+		{
+			desc: "unary mutator invalidates cache",
+			invokeRPC: func(t *testing.T, conn *grpc.ClientConn) {
+				_, err := testdata.NewTestServiceClient(conn).ClientUnaryRepoMutator(ctx, &testdata.Request{
+					Destination: repo,
+				})
+				require.NoError(t, err)
+			},
+			expectedInvalidations: []*gitalypb.Repository{repo},
+		},
+		{
+			desc: "health check does not invalidate cache",
+			invokeRPC: func(t *testing.T, conn *grpc.ClientConn) {
+				_, err := grpc_health_v1.NewHealthClient(conn).Check(ctx, &grpc_health_v1.HealthCheckRequest{
+					Service: "TestService",
+				})
+				require.NoError(t, err)
+			},
+		},
+		{
+			desc: "intercepted method does not invalidate cache",
+			invokeRPC: func(t *testing.T, conn *grpc.ClientConn) {
+				_, err := testdata.NewInterceptedServiceClient(conn).IgnoredMethod(ctx, &testdata.Request{})
+				require.NoError(t, err)
+			},
+		},
+	} {
+		t.Run(tc.desc, func(t *testing.T) {
+			mockCache := newMockCache()
+			registry, err := protoregistry.NewFromPaths("middleware/cache/testdata/stream.proto")
+			require.NoError(t, err)
+
+			server := grpc.NewServer(
+				grpc.StreamInterceptor(StreamInvalidator(mockCache, registry)),
+				grpc.UnaryInterceptor(UnaryInvalidator(mockCache, registry)),
+			)
+
+			service := &testService{
+				requestCh: make(chan bool, 1),
+			}
+			testdata.RegisterTestServiceServer(server, service)
+			testdata.RegisterInterceptedServiceServer(server, service)
+			grpc_health_v1.RegisterHealthServer(server, service)
+
+			listener, err := net.Listen("tcp", ":0")
+			require.NoError(t, err)
+			go func() {
+				testhelper.MustServe(t, server, listener)
+			}()
+			t.Cleanup(server.Stop)
+
+			conn, err := grpc.DialContext(
+				ctx,
+				listener.Addr().String(),
+				grpc.WithBlock(),
+				grpc.WithTransportCredentials(insecure.NewCredentials()),
+			)
+			require.NoError(t, err)
+
+			tc.invokeRPC(t, conn)
+
+			// Check that the RPC has been executed.
+			require.True(t, <-service.requestCh)
+			// Verify that the repository's cache was invalidated as expected.
+			testhelper.ProtoEqual(t, tc.expectedInvalidations, mockCache.invalidatedRepos)
+			// Verify that all leases that were created have ended.
+			require.Equal(t, len(mockCache.invalidatedRepos), mockCache.endedLeases.count)
+			// And furthermore, verify that we didn't see an error.
+			require.Empty(t, MethodErrCount.Method)
+		})
 	}
-
-	repo3 := &gitalypb.Repository{
-		GitAlternateObjectDirectories: []string{"3"},
-		GitObjectDirectory:            "3",
-		GlProjectPath:                 "3",
-		GlRepository:                  "3",
-		RelativePath:                  "3",
-		StorageName:                   "3",
-	}
-
-	expectedSvcRequests := []*gitalypb.Repository{repo1, repo1, repo2, repo3, repo1, repo2, repo2}
-	expectedInvalidations := []*gitalypb.Repository{repo2, repo3, repo1}
-
-	// Should NOT trigger cache invalidation
-	c, err := cli.ClientStreamRepoAccessor(ctx, &testdata.Request{
-		Destination: repo1,
-	})
-	assert.NoError(t, err)
-	_, err = c.Recv() // make client call synchronous by waiting for close
-	assert.Equal(t, err, io.EOF)
-
-	// Should NOT trigger cache invalidation
-	c, err = cli.ClientStreamRepoMaintainer(ctx, &testdata.Request{
-		Destination: repo1,
-	})
-	assert.NoError(t, err)
-	_, err = c.Recv() // make client call synchronous by waiting for close
-	assert.Equal(t, err, io.EOF)
-
-	// Should trigger cache invalidation
-	c, err = cli.ClientStreamRepoMutator(ctx, &testdata.Request{
-		Destination: repo2,
-	})
-	assert.NoError(t, err)
-	_, err = c.Recv() // make client call synchronous by waiting for close
-	assert.Equal(t, err, io.EOF)
-
-	// Should trigger cache invalidation
-	c, err = cli.ClientStreamRepoMutator(ctx, &testdata.Request{
-		Destination: repo3,
-	})
-	assert.NoError(t, err)
-	_, err = c.Recv() // make client call synchronous by waiting for close
-	assert.Equal(t, err, io.EOF)
-
-	// Should trigger cache invalidation
-	_, err = cli.ClientUnaryRepoMutator(ctx, &testdata.Request{
-		Destination: repo1,
-	})
-	require.NoError(t, err)
-
-	// Should NOT trigger cache invalidation
-	_, err = cli.ClientUnaryRepoAccessor(ctx, &testdata.Request{
-		Destination: repo2,
-	})
-	require.NoError(t, err)
-
-	// Should NOT trigger cache invalidation
-	_, err = cli.ClientUnaryRepoMaintainer(ctx, &testdata.Request{
-		Destination: repo2,
-	})
-	require.NoError(t, err)
-
-	// Health checks should NOT trigger cache invalidation
-	hcr := &grpc_health_v1.HealthCheckRequest{Service: "TestService"}
-	_, err = grpc_health_v1.NewHealthClient(cc).Check(ctx, hcr)
-	require.NoError(t, err)
-	require.Equal(t, 0, MethodErrCount.Method["/grpc.health.v1.Health/Check"])
-
-	_, err = testdata.NewInterceptedServiceClient(cc).IgnoredMethod(ctx, &testdata.Request{})
-	testhelper.RequireGrpcError(t, status.Error(codes.Unimplemented, "method IgnoredMethod not implemented"), err)
-	require.Equal(t, 0, MethodErrCount.Method["/testdata.InterceptedService/IgnoredMethod"])
-
-	testhelper.ProtoEqual(t, expectedInvalidations, mCache.(*mockCache).invalidatedRepos)
-	testhelper.ProtoEqual(t, expectedSvcRequests, svc.repoRequests)
-	require.Equal(t, 3, mCache.(*mockCache).endedLeases.count)
 }
 
 // mockCache allows us to relay back via channel which repos are being
@@ -151,7 +177,7 @@ type mockCache struct {
 	}
 }
 
-func newMockCache() diskcache.Invalidator {
+func newMockCache() *mockCache {
 	return &mockCache{
 		endedLeases: &struct {
 			sync.RWMutex
@@ -173,69 +199,49 @@ func (mc *mockCache) StartLease(repo *gitalypb.Repository) (diskcache.LeaseEnder
 	return mc, nil
 }
 
-func newTestSvc(tb testing.TB, ctx context.Context, srvr *grpc.Server, svc testdata.TestServiceServer) (testdata.TestServiceClient, *grpc.ClientConn, func()) {
-	healthSrvr := health.NewServer()
-	grpc_health_v1.RegisterHealthServer(srvr, healthSrvr)
-	healthSrvr.SetServingStatus("TestService", grpc_health_v1.HealthCheckResponse_SERVING)
-	testdata.RegisterTestServiceServer(srvr, svc)
-	testdata.RegisterInterceptedServiceServer(srvr, &testdata.UnimplementedInterceptedServiceServer{})
-
-	lis, err := net.Listen("tcp", ":0")
-	require.NoError(tb, err)
-
-	errQ := make(chan error)
-
-	go func() {
-		errQ <- srvr.Serve(lis)
-	}()
-
-	cleanup := func() {
-		srvr.Stop()
-		require.NoError(tb, <-errQ)
-	}
-
-	cc, err := grpc.DialContext(
-		ctx,
-		lis.Addr().String(),
-		grpc.WithBlock(),
-		grpc.WithTransportCredentials(insecure.NewCredentials()),
-	)
-	require.NoError(tb, err)
-
-	return testdata.NewTestServiceClient(cc), cc, cleanup
-}
-
-type testSvc struct {
+type testService struct {
 	testdata.UnimplementedTestServiceServer
-	repoRequests []*gitalypb.Repository
+	testdata.UnimplementedInterceptedServiceServer
+	grpc_health_v1.UnimplementedHealthServer
+	requestCh chan bool
 }
 
-func (ts *testSvc) ClientStreamRepoMutator(req *testdata.Request, _ testdata.TestService_ClientStreamRepoMutatorServer) error {
-	ts.repoRequests = append(ts.repoRequests, req.GetDestination())
+func (ts *testService) ClientStreamRepoMutator(*testdata.Request, testdata.TestService_ClientStreamRepoMutatorServer) error {
+	ts.requestCh <- true
 	return nil
 }
 
-func (ts *testSvc) ClientStreamRepoAccessor(req *testdata.Request, _ testdata.TestService_ClientStreamRepoAccessorServer) error {
-	ts.repoRequests = append(ts.repoRequests, req.GetDestination())
+func (ts *testService) ClientStreamRepoAccessor(*testdata.Request, testdata.TestService_ClientStreamRepoAccessorServer) error {
+	ts.requestCh <- true
 	return nil
 }
 
-func (ts *testSvc) ClientStreamRepoMaintainer(req *testdata.Request, _ testdata.TestService_ClientStreamRepoMaintainerServer) error {
-	ts.repoRequests = append(ts.repoRequests, req.GetDestination())
+func (ts *testService) ClientStreamRepoMaintainer(*testdata.Request, testdata.TestService_ClientStreamRepoMaintainerServer) error {
+	ts.requestCh <- true
 	return nil
 }
 
-func (ts *testSvc) ClientUnaryRepoMutator(_ context.Context, req *testdata.Request) (*testdata.Response, error) {
-	ts.repoRequests = append(ts.repoRequests, req.GetDestination())
+func (ts *testService) ClientUnaryRepoMutator(context.Context, *testdata.Request) (*testdata.Response, error) {
+	ts.requestCh <- true
 	return &testdata.Response{}, nil
 }
 
-func (ts *testSvc) ClientUnaryRepoAccessor(_ context.Context, req *testdata.Request) (*testdata.Response, error) {
-	ts.repoRequests = append(ts.repoRequests, req.GetDestination())
+func (ts *testService) ClientUnaryRepoAccessor(context.Context, *testdata.Request) (*testdata.Response, error) {
+	ts.requestCh <- true
 	return &testdata.Response{}, nil
 }
 
-func (ts *testSvc) ClientUnaryRepoMaintainer(_ context.Context, req *testdata.Request) (*testdata.Response, error) {
-	ts.repoRequests = append(ts.repoRequests, req.GetDestination())
+func (ts *testService) ClientUnaryRepoMaintainer(context.Context, *testdata.Request) (*testdata.Response, error) {
+	ts.requestCh <- true
+	return &testdata.Response{}, nil
+}
+
+func (ts *testService) Check(context.Context, *grpc_health_v1.HealthCheckRequest) (*grpc_health_v1.HealthCheckResponse, error) {
+	ts.requestCh <- true
+	return &grpc_health_v1.HealthCheckResponse{}, nil
+}
+
+func (ts *testService) IgnoredMethod(context.Context, *testdata.Request) (*testdata.Response, error) {
+	ts.requestCh <- true
 	return &testdata.Response{}, nil
 }
