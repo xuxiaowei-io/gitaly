846a1527172c5fd4d358fc6c256d78d4b7be2d25
Patrick Steinhardt <psteinhardt@gitlab.com>
2023-01-19 10:26:09 +0100

    housekeeping: Report when writing multi-pack-indices
    While we already have the infrastructure to write multi-pack-indices, we
    don't yet report when we have done so. Fix this by adding the respective
    fields to the map of optimizations.

--- internal/git/housekeeping/optimize_repository.go
+++ internal/git/housekeeping/optimize_repository.go
@@ -150,6 +150,7 @@ func optimizeRepository(
 		optimizations["packed_objects_full"] = "failure"
 		optimizations["packed_objects_incremental"] = "failure"
 		optimizations["written_bitmap"] = "failure"
+		optimizations["written_multi_pack_index"] = "failure"
 		return fmt.Errorf("could not repack: %w", err)
 	}
 	if didRepack {
@@ -160,6 +161,9 @@ func optimizeRepository(
 		}
 		if repackCfg.WriteBitmap {
 			optimizations["written_bitmap"] = "success"
+		}
+		if repackCfg.WriteMultiPackIndex {
+			optimizations["written_multi_pack_index"] = "success"
 		}
 	}
 	timer.ObserveDuration()
