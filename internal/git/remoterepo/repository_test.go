f6bc8d62e46a09efdef654f608cc766bd5a1d79b
Patrick Steinhardt <psteinhardt@gitlab.com>
2023-02-01 15:31:26 +0100

    remoterepo: Split out test helper to set up Gitaly server
    Split out a test helper to set up the Gitaly server, which will be
    required in a new test case we're about to add. While at it, adjust the
    "helper_test.go" file to be called "testhelper_test.go" as it is
    customary in the Gitaly project.

--- internal/git/remoterepo/repository_test.go
+++ internal/git/remoterepo/repository_test.go
@@ -11,46 +11,14 @@ import (
 	"gitlab.com/gitlab-org/gitaly/v15/internal/git"
 	"gitlab.com/gitlab-org/gitaly/v15/internal/git/gittest"
 	"gitlab.com/gitlab-org/gitaly/v15/internal/git/remoterepo"
-	"gitlab.com/gitlab-org/gitaly/v15/internal/gitaly/service"
-	"gitlab.com/gitlab-org/gitaly/v15/internal/gitaly/service/commit"
-	"gitlab.com/gitlab-org/gitaly/v15/internal/gitaly/service/ref"
-	"gitlab.com/gitlab-org/gitaly/v15/internal/gitaly/service/repository"
 	"gitlab.com/gitlab-org/gitaly/v15/internal/gitaly/storage"
 	"gitlab.com/gitlab-org/gitaly/v15/internal/metadata"
-	"gitlab.com/gitlab-org/gitaly/v15/internal/testhelper/testcfg"
-	"gitlab.com/gitlab-org/gitaly/v15/internal/testhelper/testserver"
-	"gitlab.com/gitlab-org/gitaly/v15/proto/go/gitalypb"
-	"google.golang.org/grpc"
 )
 
 func TestRepository(t *testing.T) {
-	cfg := testcfg.Build(t)
+	t.Parallel()
 
-	cfg.SocketPath = testserver.RunGitalyServer(t, cfg, nil, func(srv *grpc.Server, deps *service.Dependencies) {
-		gitalypb.RegisterRepositoryServiceServer(srv, repository.NewServer(
-			deps.GetCfg(),
-			deps.GetRubyServer(),
-			deps.GetLocator(),
-			deps.GetTxManager(),
-			deps.GetGitCmdFactory(),
-			deps.GetCatfileCache(),
-			deps.GetConnsPool(),
-			deps.GetGit2goExecutor(),
-			deps.GetHousekeepingManager(),
-		))
-		gitalypb.RegisterCommitServiceServer(srv, commit.NewServer(
-			deps.GetCfg(),
-			deps.GetLocator(),
-			deps.GetGitCmdFactory(),
-			deps.GetCatfileCache(),
-		))
-		gitalypb.RegisterRefServiceServer(srv, ref.NewServer(
-			deps.GetLocator(),
-			deps.GetGitCmdFactory(),
-			deps.GetTxManager(),
-			deps.GetCatfileCache(),
-		))
-	})
+	cfg := setupGitalyServer(t)
 
 	pool := client.NewPool()
 	defer pool.Close()
