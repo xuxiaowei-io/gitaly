3060313e355e2fe711f6964a44ae5335fb68a76c
James Fargher <jfargher@gitlab.com>
2023-02-08 09:11:54 +1300

    perm: Add constants for files and executables
    This is in preparation for a bulk permission replace.

--- internal/helper/perm/perm.go
+++ internal/helper/perm/perm.go
@@ -22,4 +22,28 @@ const (
 	// PublicDir is the permission given for a directory that may be read or
 	// written outside of gitaly.
 	PublicDir fs.FileMode = 0o777
+
+	// PrivateFile is the permissions given for a file that must only be used
+	// by gitaly.
+	PrivateFile fs.FileMode = 0o600
+
+	// SharedFile is the permission given for a file that may be read outside
+	// of gitaly.
+	SharedFile fs.FileMode = 0o644
+
+	// PublicFile is the permission given for a file that may be read or
+	// written outside of gitaly.
+	PublicFile fs.FileMode = 0o666
+
+	// PrivateExecutable is the permissions given for an executable that must
+	// only be used by gitaly.
+	PrivateExecutable fs.FileMode = 0o700
+
+	// SharedExecutable is the permission given for an executable that may be
+	// executed outside of gitaly.
+	SharedExecutable fs.FileMode = 0o755
+
+	// PublicExecutable is the permission given for an executable that may be
+	// read or written outside of gitaly.
+	PublicExecutable fs.FileMode = 0o777
 )
