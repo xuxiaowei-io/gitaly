01930dff6714cd9b904163a50658609c9ad24d75
James Fargher <jfargher@gitlab.com>
2023-02-13 15:37:04 +1300

    gitaly-backup: Inject GITALY_SERVERS env var
    Now that each backup task, create/restore, will accept gitaly connection
    information off of either the context or the request message we inject
    an environment variable to allow globally setting gitaly connection
    information.
    
    This global configuration will later be used to clear each storage
    before starting a restore via RemoveAll.
    
    Changelog: added

--- cmd/gitaly-backup/main.go
+++ cmd/gitaly-backup/main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"io"
 	"os"
 
+	"gitlab.com/gitlab-org/gitaly/v15/internal/gitaly/storage"
+	"gitlab.com/gitlab-org/gitaly/v15/internal/helper/env"
 	"gitlab.com/gitlab-org/gitaly/v15/internal/log"
+	"google.golang.org/grpc/metadata"
 )
 
 type subcmd interface {
@@ -41,7 +45,29 @@ func main() {
 	subcmd.Flags(subcmdFlags)
 	_ = subcmdFlags.Parse(flags.Args()[2:])
 
-	if err := subcmd.Run(context.Background(), os.Stdin, os.Stdout); err != nil {
+	ctx, err := injectGitalyServersEnv(context.Background())
+	if err != nil {
 		logger.Fatalf("%s", err)
 	}
+
+	if err := subcmd.Run(ctx, os.Stdin, os.Stdout); err != nil {
+		logger.Fatalf("%s", err)
+	}
+}
+
+func injectGitalyServersEnv(ctx context.Context) (context.Context, error) {
+	rawServers := env.GetString("GITALY_SERVERS", "")
+	if rawServers == "" {
+		return ctx, nil
+	}
+
+	md := metadata.Pairs("gitaly-servers", rawServers)
+	ctx = metadata.NewIncomingContext(ctx, md)
+
+	// Make sure we fail early if the value in the env var cannot be interpreted.
+	if _, err := storage.ExtractGitalyServers(ctx); err != nil {
+		return nil, fmt.Errorf("injecting GITALY_SERVERS: %w", err)
+	}
+
+	return ctx, nil
 }
