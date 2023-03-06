24aeeb4e59539dff47bd040127e0e7effacf8893
John Cai <jcai@gitlab.com>
2023-03-06 18:31:05 +0000

    Revert "Merge branch 'jc-submodule-in-git-fix' into 'master'"
    This reverts merge request !5448

--- internal/metadata/featureflag/ff_submodule_in_git.go
+++
@@ -1,9 +0,0 @@
-package featureflag
-
-// SubmoduleInGit will enable the use of Git v2.38
-var SubmoduleInGit = NewFeatureFlag(
-	"submodule_in_git",
-	"v15.10.0",
-	"https://gitlab.com/gitlab-org/gitaly/-/issues/4801",
-	false,
-)
