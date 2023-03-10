3362ba8e42394eb9051b1e6df1b0a91657e72cde
Sami Hiltunen <shiltunen@gitlab.com>
2023-03-10 22:37:52 +0200

    Add helpers for fsyncing
    This commit adds helpers for:
    
    1. fsyncing a given file/directory. This is useful when there's no
       open file handle yet.
    2. fsyncing a file hierarchy upwards. This useful when a directory
       hierarchy has been created without fsyncing the entries.
    3. fsyncing a directory recursivel. This is useful fsyncing a
       directory and it's children.
    
    These will be later useful when fsyncing an extracted tar as tar
    doesn't fsync them itself.

---
+++ internal/safe/sync.go
@@ -0,0 +1,81 @@
+package safe
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+type file interface {
+	Sync() error
+	Close() error
+}
+
+// Syncer implements helper methods for fsyncing files and directories.
+type Syncer struct {
+	// open is used by the Sync* functions to open files for syncing. By default, Syncer opens
+	// files via the `os` package as usual. This is parametrized only so we can test the correct
+	// calls were made as we don't have good way to test the fsyncing otherwise.
+	open func(string) (file, error)
+}
+
+// NewSyncer returns a new Syncer.
+func NewSyncer() Syncer {
+	return Syncer{
+		open: func(path string) (file, error) {
+			return os.Open(path)
+		},
+	}
+}
+
+// Sync opens the file/directory at the given path and syncs it. Syncing a directory syncs just
+// the directory itself, not the children. If the children need to be synced as well, use SyncRecursive.
+func (s Syncer) Sync(path string) error {
+	f, err := s.open(path)
+	if err != nil {
+		return fmt.Errorf("open: %w", err)
+	}
+	defer func() { _ = f.Close() }()
+
+	if err := f.Sync(); err != nil {
+		return fmt.Errorf("sync: %w", err)
+	}
+
+	if err := f.Close(); err != nil {
+		return fmt.Errorf("close: %w", err)
+	}
+
+	return nil
+}
+
+// SyncRecursive walks the file tree rooted at path and fsyncing the root and the children.
+func (s Syncer) SyncRecursive(path string) error {
+	return filepath.WalkDir(path, func(path string, _ os.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		return s.Sync(path)
+	})
+}
+
+// SyncHierarchy walks the hierarchy from <rootPath>/<relativePath> up to the
+// rootPath and syncs everything on the way. If the path points to a directory,
+// it only syncs from the directory and upwards, not the contents of the directory.
+func (s Syncer) SyncHierarchy(rootPath, relativePath string) error {
+	// Traverse the directory hierarchy all the way up to root and fsync them.
+	currentPath := filepath.Join(rootPath, relativePath)
+	for {
+		if err := s.Sync(currentPath); err != nil {
+			return fmt.Errorf("sync: %w", err)
+		}
+
+		if currentPath == rootPath {
+			break
+		}
+
+		currentPath = filepath.Dir(currentPath)
+	}
+
+	return nil
+}
---
+++ internal/safe/sync_test.go
@@ -0,0 +1,194 @@
+package safe
+
+import (
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+type call struct {
+	method string
+	path   string
+}
+
+// recordingOpener opens files normally through the OS package but
+// it records all of the calls for testing.
+type recordingOpener struct {
+	wrappedOpen func(string) (file, error)
+	calls       []call
+}
+
+func (r *recordingOpener) recordedCalls(trimPrefix string) []call {
+	var calls []call
+	for _, c := range r.calls {
+		calls = append(calls, call{
+			method: c.method,
+			path:   strings.TrimPrefix(c.path, trimPrefix),
+		})
+	}
+
+	return calls
+}
+
+func (r *recordingOpener) open(path string) (file, error) {
+	r.record("open", path)
+
+	f, err := r.wrappedOpen(path)
+	if err != nil {
+		return nil, err
+	}
+
+	return &recordingFile{
+		record: func(method string) { r.record(method, path) },
+		file:   f,
+	}, nil
+}
+
+func (r *recordingOpener) record(method, path string) {
+	r.calls = append(r.calls, call{
+		method: method,
+		path:   path,
+	})
+}
+
+type recordingFile struct {
+	record func(string)
+	file
+}
+
+func (r *recordingFile) Sync() error {
+	r.record("sync")
+	return r.file.Sync()
+}
+
+func (r *recordingFile) Close() error {
+	r.record("close")
+	return r.file.Close()
+}
+
+// recordingSyncer returns a new Syncer and a recordingOpener
+// that records the open calls made by the Sync* functions.
+func recordingSyncer() (Syncer, *recordingOpener) {
+	syncer := NewSyncer()
+	recorder := &recordingOpener{wrappedOpen: syncer.open}
+	syncer.open = recorder.open
+
+	return syncer, recorder
+}
+
+func createTestDirectoryHierarchy(tb testing.TB) (string, string) {
+	tb.Helper()
+
+	tmpDir := tb.TempDir()
+	rootDir := filepath.Join(tmpDir, "root")
+	for _, dir := range []string{
+		"a/c",
+		"a/d",
+		"b/e",
+		"b/f",
+	} {
+		require.NoError(tb, os.MkdirAll(filepath.Join(rootDir, dir), fs.ModePerm))
+	}
+
+	for _, path := range []string{
+		"file_1",
+		"file_2",
+		"a/file_3",
+		"a/file_4",
+		"b/file_5",
+		"b/file_6",
+		"a/c/file_7",
+		"a/c/file_8",
+		"a/d/file_9",
+		"a/d/file_10",
+		"b/e/file_11",
+		"b/e/file_12",
+		"b/f/file_13",
+		"b/f/file_14",
+	} {
+		require.NoError(tb, os.WriteFile(
+			filepath.Join(rootDir, path), nil, fs.ModePerm,
+		))
+	}
+
+	return tmpDir, rootDir
+}
+
+func expectedCalls(paths ...string) []call {
+	var calls []call
+	for _, path := range paths {
+		calls = append(calls, []call{
+			{method: "open", path: path},
+			{method: "sync", path: path},
+			{method: "close", path: path},
+			// The second close is the deferred file close.
+			{method: "close", path: path},
+		}...)
+	}
+
+	return calls
+}
+
+func TestSync(t *testing.T) {
+	for _, tc := range []struct {
+		desc          string
+		path          string
+		expectedCalls []call
+	}{
+		{
+			desc: "file",
+			path: "/root/a/file_3",
+		},
+		{
+			desc: "directory",
+			path: "/root/a/c",
+		},
+	} {
+		t.Run(tc.desc, func(t *testing.T) {
+			tmpDir, _ := createTestDirectoryHierarchy(t)
+
+			syncer, recorder := recordingSyncer()
+			require.NoError(t, syncer.Sync(filepath.Join(tmpDir, tc.path)))
+			require.Equal(t, expectedCalls(tc.path), recorder.recordedCalls(tmpDir))
+		})
+	}
+}
+
+func TestSyncHierarchy(t *testing.T) {
+	tmpDir, rootDir := createTestDirectoryHierarchy(t)
+	syncer, recorder := recordingSyncer()
+	require.NoError(t, syncer.SyncHierarchy(rootDir, "a/c/file_7"))
+	require.Equal(t,
+		expectedCalls(
+			"/root/a/c/file_7",
+			"/root/a/c",
+			"/root/a",
+			"/root",
+		),
+		recorder.recordedCalls(tmpDir),
+	)
+}
+
+func TestSyncRecursive(t *testing.T) {
+	tmpDir, rootDir := createTestDirectoryHierarchy(t)
+	syncer, recorder := recordingSyncer()
+	require.NoError(t, syncer.SyncRecursive(filepath.Join(rootDir, "a")))
+	require.Equal(t,
+		expectedCalls(
+			"/root/a",
+			"/root/a/c",
+			"/root/a/c/file_7",
+			"/root/a/c/file_8",
+			"/root/a/d",
+			"/root/a/d/file_10",
+			"/root/a/d/file_9",
+			"/root/a/file_3",
+			"/root/a/file_4",
+		),
+		recorder.recordedCalls(tmpDir),
+	)
+}
