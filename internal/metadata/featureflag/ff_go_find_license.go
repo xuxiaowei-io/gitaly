dc763c3e91b2ac311819e434033ed7b4a29e5b47
Toon Claes <toon@gitlab.com>
2023-03-14 09:14:52 +0100

    repository: Remove use of Ruby FindLicense
    The roll-out of the Go implementation for FindLicense has been going
    well. This change removes the code that calls into the Ruby
    implementation and the feature flag that was toggling that behavior.
    
    Issue: https://gitlab.com/gitlab-org/gitaly/-/issues/3759

--- internal/metadata/featureflag/ff_go_find_license.go
+++
@@ -1,9 +0,0 @@
-package featureflag
-
-// GoFindLicense enables Go implementation of FindLicense
-var GoFindLicense = NewFeatureFlag(
-	"go_find_license",
-	"v14.3.0",
-	"https://gitlab.com/gitlab-org/gitaly/-/issues/3759",
-	false,
-)
