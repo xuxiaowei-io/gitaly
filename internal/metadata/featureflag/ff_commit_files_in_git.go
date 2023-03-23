2bab5aad22e0d15888d1b91573b33cc13d959e4d
John Cai <jcai@gitlab.com>
2023-03-23 16:00:26 -0400

    featureflag: Add CommitFilesInGit feature flag

---
+++ internal/metadata/featureflag/ff_commit_files_in_git.go
@@ -0,0 +1,10 @@
+package featureflag
+
+// CommitFilesInGit enables implementation of UserCommitFiles using
+// git plumbing commands instead of git2go
+var CommitFilesInGit = NewFeatureFlag(
+	"commit_files_in_git",
+	"15.11.0",
+	"https://gitlab.com/gitlab-org/gitaly/-/issues/5019",
+	false,
+)
