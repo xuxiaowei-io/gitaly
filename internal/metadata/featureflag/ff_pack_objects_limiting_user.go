c4397165c73e9ad4a4640cbf771ba2155c6e7ab1
Quang-Minh Nguyen <qmnguyen@gitlab.com>
2023-03-29 14:57:56 +0700

    Add pack-objects concurrency limiter based on RemoteIP
    This commits adds one more concurrency limiting strategy for
    pack-objects. This strategy depends on RemoteIP passed from the
    downstream services. As the foundation for concurrency limiting is
    solid, the change is minimal. This commit just needs to declares a key
    before triggering `runPackObjectsLimited` method. Other observability
    stuff are included automatically like magic.

--- internal/metadata/featureflag/ff_pack_objects_limiting_user.go
+++ internal/metadata/featureflag/ff_pack_objects_limiting_user.go
@@ -4,7 +4,7 @@ package featureflag
 // based off of the user
 var PackObjectsLimitingUser = NewFeatureFlag(
 	"pack_objects_limiting_user",
-	"v15.6.0",
+	"v15.11.0",
 	"https://gitlab.com/gitlab-org/gitaly/-/issues/4413",
 	false,
 )
