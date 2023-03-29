5790d65abc7f99833152c472ea22b1a91bc297cf
Pavlo Strokov <pstrokov@gitlab.com>
2023-03-29 17:10:21 +0300

    Extract common code for validation
    Because Praefect and Gitaly use the same strategy for configuration
    processing it makes sense to move code that can be re-used in both
    to a shared place and omit duplication.

--- internal/cli/gitaly/configuration-validate.go
+++ internal/cli/gitaly/configuration-validate.go
@@ -1,36 +1,13 @@
 package gitaly
 
 import (
-	"encoding/json"
-	"errors"
-	"fmt"
-	"io"
-
-	"github.com/pelletier/go-toml/v2"
 	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli/v2"
-	"gitlab.com/gitlab-org/gitaly/v15/internal/errors/cfgerror"
+	"gitlab.com/gitlab-org/gitaly/v15/cmd"
 	"gitlab.com/gitlab-org/gitaly/v15/internal/gitaly/config"
 )
 
 const validationErrorCode = 2
-
-type validationOutputError struct {
-	Key     []string `json:"key,omitempty"`
-	Message string   `json:"message,omitempty"`
-}
-
-type validationOutput struct {
-	Errors []validationOutputError `json:"errors,omitempty"`
-}
-
-func jsonEncoded(outStream io.Writer, errStream io.Writer, indent string, val any) {
-	encoder := json.NewEncoder(outStream)
-	encoder.SetIndent("", indent)
-	if err := encoder.Encode(val); err != nil {
-		fmt.Fprintf(errStream, "writing results: %v\n", err)
-	}
-}
 
 func newConfigurationCommand() *cli.Command {
 	return &cli.Command{
@@ -51,29 +28,14 @@ func validateConfigurationAction(ctx *cli.Context) error {
 
 	cfg, err := config.Load(ctx.App.Reader)
 	if err != nil {
-		terr := &toml.DecodeError{}
-		if errors.As(err, &terr) {
-			row, column := terr.Position()
-			jsonEncoded(ctx.App.Writer, ctx.App.ErrWriter, "  ", validationOutput{Errors: []validationOutputError{{
-				Key:     terr.Key(),
-				Message: fmt.Sprintf("line %d column %d: %v", row, column, terr.Error()),
-			}}})
+		if cmd.WriteTomlReadError(err, ctx.App.Writer, ctx.App.ErrWriter) {
 			return cli.Exit("", validationErrorCode)
 		}
 
-		return cli.Exit(fmt.Sprintf("processing input data: %v", err), 1)
+		return cli.Exit("", 1)
 	}
 
-	out := validationOutput{}
-	for _, err := range cfgerror.New().Append(cfg.ValidateV2()) {
-		out.Errors = append(out.Errors, validationOutputError{
-			Key:     err.Key,
-			Message: err.Cause.Error(),
-		})
-	}
-
-	if len(out.Errors) > 0 {
-		jsonEncoded(ctx.App.Writer, ctx.App.ErrWriter, "  ", out)
+	if !cmd.Validate(&cfg, ctx.App.Writer, ctx.App.ErrWriter) {
 		return cli.Exit("", validationErrorCode)
 	}
 
