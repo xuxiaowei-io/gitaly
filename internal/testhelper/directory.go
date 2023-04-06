c832f426951a3ed6c90bbd6e8a95454ae9ff7480
Sami Hiltunen <shiltunen@gitlab.com>
2023-04-06 19:56:27 +0300

    Support parsing content in RequireDirectoryState
    RequireDirectoryState allows for asserting that a directory matches
    the expected state. Right now it only supports matching file content
    against expected bytes. This is inconvenient as it can be difficult
    to make sense of binary file formats on failure and say what exactly
    is wrong without looking at the file itself. Further, some files may
    be physically different but semantically the same. An example of such
    a file would be Git's pack files, where the physical format may change
    for example depending on the delta compression but the actual contained
    objects are the same.
    
    This commit adds support for parsing the file's content with a parser
    passed in the assertion. The parser takes the file's contents and returns
    a parsed object of any type. The parsed type is ultimately asserted for
    equality with the expected content. With this in place, we can for example
    assert the content of pack files rather than than the physical file bytes.

--- internal/testhelper/directory.go
+++ internal/testhelper/directory.go
@@ -19,7 +19,11 @@ type DirectoryEntry struct {
 	// Mode is the file mode of the entry.
 	Mode fs.FileMode
 	// Content contains the file content if this is a regular file.
-	Content []byte
+	Content      any
+	// ParseContent is a function that receives the file's actual content and
+	// returns it parsed into the expected form. The returned value is ultimately
+	// asserted for equality with the Content.
+	ParseContent func(tb testing.TB, content []byte) any
 }
 
 // DirectoryState models the contents of a directory. The key is relative of the entry in
@@ -56,8 +60,13 @@ func RequireDirectoryState(tb testing.TB, rootDirectory, relativeDirectory strin
 		}
 
 		if entry.Type().IsRegular() {
-			actualEntry.Content, err = os.ReadFile(path)
+			content, err := os.ReadFile(path)
 			require.NoError(tb, err)
+
+			actualEntry.Content = content
+			if expectedEntry, ok := expected[trimmedPath]; ok && expectedEntry.ParseContent != nil {
+				actualEntry.Content = expectedEntry.ParseContent(tb, content)
+			}
 		}
 
 		actual[trimmedPath] = actualEntry
@@ -65,11 +74,15 @@ func RequireDirectoryState(tb testing.TB, rootDirectory, relativeDirectory strin
 		return nil
 	}))
 
-	if expected == nil {
-		expected = DirectoryState{}
+	// Create a copy of the expected and set the ParseContent to nil as functions always fail
+	// equality checks. We use a copy so we don't unexpectedly modify the original.
+	expectedCopy := make(DirectoryState, len(expected))
+	for key, value := range expected {
+		value.ParseContent = nil
+		expectedCopy[key] = value
 	}
 
-	require.Equal(tb, expected, actual)
+	require.Equal(tb, expectedCopy, actual)
 }
 
 // RequireTarState asserts that the provided tarball contents matches the expected state.
