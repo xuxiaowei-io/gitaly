57565c650437d32957fa06d4cc94ba9bd2e666e2
John Cai <jcai@gitlab.com>
2023-04-20 15:17:37 -0400

    featureflag: Delete GitV240
    GitV240 has been completely rolled out in production. Remove it, as well
    as the bundled git environment for 2.39 so that in a subsequent change
    we can bump the minimum version to v2.40

--- internal/metadata/featureflag/ff_git_v240.go
+++
@@ -1,9 +0,0 @@
-package featureflag
-
-// GitV240 enables the use of Git v2.40.
-var GitV240 = NewFeatureFlag(
-	"git_v240",
-	"v15.11.0",
-	"https://gitlab.com/gitlab-org/gitaly/-/issues/5030",
-	false,
-)
