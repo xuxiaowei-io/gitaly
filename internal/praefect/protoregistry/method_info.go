e12acb508bdfb85f8a15c3e9c714848d1d214993
Patrick Steinhardt <psteinhardt@gitlab.com>
2023-04-25 10:00:38 +0200

    protoregistry: Remove old generic-reflection based logic
    We have converted the code that uses generic reflection-based logic to
    access Protobuf fields tagged with extensions to instead use the tools
    provided by Protobuf. These require less dark elf magic and are a bit
    easier to understand than the previous code.
    
    Remove the old and now-unused logic that uses generic reflection to find
    Protobuf fields.

--- internal/praefect/protoregistry/method_info.go
+++ internal/praefect/protoregistry/method_info.go
@@ -8,7 +8,6 @@ import (
 	"gitlab.com/gitlab-org/gitaly/v15/internal/protoutil"
 	"gitlab.com/gitlab-org/gitaly/v15/proto/go/gitalypb"
 	"google.golang.org/protobuf/proto"
-	"google.golang.org/protobuf/reflect/protodesc"
 	"google.golang.org/protobuf/reflect/protopath"
 	"google.golang.org/protobuf/reflect/protorange"
 	"google.golang.org/protobuf/reflect/protoreflect"
@@ -66,11 +65,8 @@ var protoScope = map[gitalypb.OperationMsg_Scope]Scope{
 type MethodInfo struct {
 	Operation      OpType
 	Scope          Scope
-	targetRepo     []int
-	additionalRepo []int
 	requestName    string // protobuf message name for input type
 	requestFactory protoFactory
-	storage        []int
 	fullMethodName string
 }
 
@@ -249,168 +245,7 @@ func parseMethodInfo(
 		fullMethodName: fullMethodName,
 	}
 
-	topLevelMsgs, err := getTopLevelMsgs(p)
-	if err != nil {
-		return MethodInfo{}, err
-	}
-
-	typeName, err := lastName(methodDesc.GetInputType())
-	if err != nil {
-		return MethodInfo{}, err
-	}
-
-	if scope == ScopeRepository {
-		m := matcher{
-			match:        protoutil.GetTargetRepositoryExtension,
-			subMatch:     protoutil.GetRepositoryExtension,
-			expectedType: ".gitaly.Repository",
-			topLevelMsgs: topLevelMsgs,
-		}
-
-		targetRepo, err := m.findField(topLevelMsgs[typeName])
-		if err != nil {
-			return MethodInfo{}, err
-		}
-		if targetRepo == nil {
-			return MethodInfo{}, fmt.Errorf("unable to find target repository for method: %s", requestName)
-		}
-		mi.targetRepo = targetRepo
-
-		m.match = protoutil.GetAdditionalRepositoryExtension
-		additionalRepo, err := m.findField(topLevelMsgs[typeName])
-		if err != nil {
-			return MethodInfo{}, err
-		}
-		mi.additionalRepo = additionalRepo
-	} else if scope == ScopeStorage {
-		m := matcher{
-			match:        protoutil.GetStorageExtension,
-			topLevelMsgs: topLevelMsgs,
-		}
-		storage, err := m.findField(topLevelMsgs[typeName])
-		if err != nil {
-			return MethodInfo{}, err
-		}
-		if storage == nil {
-			return MethodInfo{}, fmt.Errorf("unable to find storage for method: %s", requestName)
-		}
-		mi.storage = storage
-	}
-
 	return mi, nil
-}
-
-func getFileTypes(filename string) ([]*descriptorpb.DescriptorProto, error) {
-	fd, err := protoreg.GlobalFiles.FindFileByPath(filename)
-	if err != nil {
-		return nil, err
-	}
-	sharedFD := protodesc.ToFileDescriptorProto(fd)
-
-	types := sharedFD.GetMessageType()
-
-	for _, dep := range sharedFD.Dependency {
-		depTypes, err := getFileTypes(dep)
-		if err != nil {
-			return nil, err
-		}
-		types = append(types, depTypes...)
-	}
-
-	return types, nil
-}
-
-func getTopLevelMsgs(p *descriptorpb.FileDescriptorProto) (map[string]*descriptorpb.DescriptorProto, error) {
-	topLevelMsgs := map[string]*descriptorpb.DescriptorProto{}
-	types, err := getFileTypes(p.GetName())
-	if err != nil {
-		return nil, err
-	}
-	for _, msg := range types {
-		topLevelMsgs[msg.GetName()] = msg
-	}
-	return topLevelMsgs, nil
-}
-
-// Matcher helps find field matching credentials. At first match method is used to check fields
-// recursively. Then if field matches but type don't match expectedType subMatch method is used
-// from this point. This matcher assumes that only one field in the message matches the credentials.
-type matcher struct {
-	match        func(*descriptorpb.FieldDescriptorProto) (bool, error)
-	subMatch     func(*descriptorpb.FieldDescriptorProto) (bool, error)
-	expectedType string                                   // fully qualified name of expected type e.g. ".gitaly.Repository"
-	topLevelMsgs map[string]*descriptorpb.DescriptorProto // Map of all top level messages in given file and it dependencies. Result of getTopLevelMsgs should be used.
-}
-
-func (m matcher) findField(t *descriptorpb.DescriptorProto) ([]int, error) {
-	for _, f := range t.GetField() {
-		match, err := m.match(f)
-		if err != nil {
-			return nil, err
-		}
-		if match {
-			if f.GetTypeName() == m.expectedType {
-				return []int{int(f.GetNumber())}, nil
-			} else if m.subMatch != nil {
-				m.match = m.subMatch
-				m.subMatch = nil
-			} else {
-				return nil, fmt.Errorf("found wrong type, expected: %s, got: %s", m.expectedType, f.GetTypeName())
-			}
-		}
-
-		childMsg, err := findChildMsg(m.topLevelMsgs, t, f)
-		if err != nil {
-			return nil, err
-		}
-
-		if childMsg != nil {
-			nestedField, err := m.findField(childMsg)
-			if err != nil {
-				return nil, err
-			}
-			if nestedField != nil {
-				return append([]int{int(f.GetNumber())}, nestedField...), nil
-			}
-		}
-	}
-	return nil, nil
-}
-
-func findChildMsg(topLevelMsgs map[string]*descriptorpb.DescriptorProto, t *descriptorpb.DescriptorProto, f *descriptorpb.FieldDescriptorProto) (*descriptorpb.DescriptorProto, error) {
-	var childType *descriptorpb.DescriptorProto
-	const msgPrimitive = "TYPE_MESSAGE"
-	if primitive := f.GetType().String(); primitive != msgPrimitive {
-		return nil, nil
-	}
-
-	msgName, err := lastName(f.GetTypeName())
-	if err != nil {
-		return nil, err
-	}
-
-	for _, nestedType := range t.GetNestedType() {
-		if msgName == nestedType.GetName() {
-			return nestedType, nil
-		}
-	}
-
-	if childType = topLevelMsgs[msgName]; childType != nil {
-		return childType, nil
-	}
-
-	return nil, fmt.Errorf("could not find message type %q", msgName)
-}
-
-func lastName(inputType string) (string, error) {
-	tokens := strings.Split(inputType, ".")
-
-	msgName := tokens[len(tokens)-1]
-	if msgName == "" {
-		return "", fmt.Errorf("unable to parse method input type: %s", inputType)
-	}
-
-	return msgName, nil
 }
 
 type valueField struct {
