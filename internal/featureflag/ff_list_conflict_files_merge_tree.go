ba17affa1de537ed4ba06d5a11fa556e84b77391
Karthik Nayak <knayak@gitlab.com>
2023-05-05 20:57:21 +0200

    featureflag: Add flag ListConflictFilesMergeTree
    Add a new flag `ListConflictFilesMergeTree`, which will be used to
    enable the use of git-merge-tree(1) for the ListConflictFiles RPC.

---
+++ internal/featureflag/ff_list_conflict_files_merge_tree.go
@@ -0,0 +1,10 @@
+package featureflag
+
+// ListConflictFilesMergeTree enables the usage of git-merge-tree(1) for
+// the ListConflictFiles RPC.
+var ListConflictFilesMergeTree = NewFeatureFlag(
+	"list_conflict_files_merge_tree",
+	"v16.0.0",
+	"https://gitlab.com/gitlab-org/gitaly/-/issues/5098",
+	false,
+)
