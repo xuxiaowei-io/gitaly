9b602eadb9e4eda4a1e43b4282318fb40ad3c49d
Sami Hiltunen <shiltunen@gitlab.com>
2023-05-10 19:09:28 +0300

    Remove storage name from partition keys
    Since all storages now have independent state for partitions, it is
    no longer necessary to prefix the partition keys with the storage
    name. Remove the storage name component from the partition keys.

--- internal/gitaly/partition_manager.go
+++ internal/gitaly/partition_manager.go
@@ -158,11 +158,6 @@ func stagingDirectoryPath(storagePath string) string {
 	return filepath.Join(storagePath, "staging")
 }
 
-// getPartitionKey returns a partitions's key.
-func getPartitionKey(storageName, relativePath string) string {
-	return storageName + ":" + relativePath
-}
-
 // Begin gets the TransactionManager for the specified repository and starts a Transaction. If a
 // TransactionManager is not already running, a new one is created and used. The partition tracks
 // the number of pending transactions and this counter gets incremented when Begin is invoked.
@@ -177,8 +172,6 @@ func (pm *PartitionManager) Begin(ctx context.Context, repo repo.GitRepo) (*Tran
 		return nil, structerr.NewInvalidArgument("validate relative path: %w", err)
 	}
 
-	partitionKey := getPartitionKey(repo.GetStorageName(), relativePath)
-
 	for {
 		storageMgr.mu.Lock()
 		if storageMgr.stopped {
@@ -186,7 +179,7 @@ func (pm *PartitionManager) Begin(ctx context.Context, repo repo.GitRepo) (*Tran
 			return nil, ErrPartitionManagerStopped
 		}
 
-		ptn, ok := storageMgr.partitions[partitionKey]
+		ptn, ok := storageMgr.partitions[relativePath]
 		if !ok {
 			ptn = &partition{
 				shutdown: make(chan struct{}),
@@ -201,7 +194,7 @@ func (pm *PartitionManager) Begin(ctx context.Context, repo repo.GitRepo) (*Tran
 			mgr := NewTransactionManager(storageMgr.database, storageMgr.path, relativePath, stagingDir, storageMgr.repoFactory, storageMgr.transactionFinalizerFactory(ptn))
 			ptn.transactionManager = mgr
 
-			storageMgr.partitions[partitionKey] = ptn
+			storageMgr.partitions[relativePath] = ptn
 
 			storageMgr.activePartitions.Add(1)
 			go func() {
@@ -214,7 +207,7 @@ func (pm *PartitionManager) Begin(ctx context.Context, repo repo.GitRepo) (*Tran
 				// deleted allowing the next transaction for the repository to create a new partition
 				// and TransactionManager.
 				storageMgr.mu.Lock()
-				delete(storageMgr.partitions, partitionKey)
+				delete(storageMgr.partitions, relativePath)
 				storageMgr.mu.Unlock()
 
 				close(ptn.shutdown)
