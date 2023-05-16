e72fbdba156475fe1831d3affa9f1ba2185525ab
Patrick Steinhardt <psteinhardt@gitlab.com>
2023-05-16 11:09:36 +0200

    testhelper: Split generation of certificates into standalone file
    Split the generation of certificates into a standalone file so that it's
    easier to discover and have related functionality grouped neatly
    together.

--- internal/testhelper/testhelper.go
+++ internal/testhelper/testhelper.go
@@ -3,15 +3,9 @@ package testhelper
 import (
 	"bytes"
 	"context"
-	"crypto/ecdsa"
-	"crypto/elliptic"
-	"crypto/rand"
-	"crypto/x509"
-	"encoding/pem"
 	"errors"
 	"fmt"
 	"io"
-	"math/big"
 	mrand "math/rand"
 	"net"
 	"net/http"
@@ -303,81 +297,6 @@ func Unsetenv(tb testing.TB, key string) {
 	// And now we can unset the environment variable given that we know we're not running in a
 	// parallel test and where the cleanup function has been installed.
 	require.NoError(tb, os.Unsetenv(key))
-}
-
-// GenerateCerts creates a certificate that can be used to establish TLS protected TCP connection.
-// It returns paths to the file with the certificate and its private key.
-func GenerateCerts(tb testing.TB) (string, string) {
-	tb.Helper()
-
-	rootCert := &x509.Certificate{
-		SerialNumber:          big.NewInt(1),
-		NotBefore:             time.Now(),
-		NotAfter:              time.Now().AddDate(0, 0, 1),
-		BasicConstraintsValid: true,
-		IsCA:                  true,
-		KeyUsage:              x509.KeyUsageCertSign,
-	}
-
-	rootKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
-	require.NoError(tb, err)
-
-	rootBytes, err := x509.CreateCertificate(rand.Reader, rootCert, rootCert, &rootKey.PublicKey, rootKey)
-	require.NoError(tb, err)
-
-	entityKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
-	require.NoError(tb, err)
-
-	entityCert := &x509.Certificate{
-		SerialNumber: big.NewInt(2),
-		NotBefore:    time.Now(),
-		NotAfter:     time.Now().AddDate(0, 0, 1),
-		IPAddresses:  []net.IP{net.ParseIP("0.0.0.0"), net.ParseIP("127.0.0.1"), net.ParseIP("::1"), net.ParseIP("::")},
-		DNSNames:     []string{"localhost"},
-		KeyUsage:     x509.KeyUsageDigitalSignature,
-		ExtKeyUsage: []x509.ExtKeyUsage{
-			x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth,
-		},
-	}
-
-	entityBytes, err := x509.CreateCertificate(rand.Reader, entityCert, rootCert, &entityKey.PublicKey, rootKey)
-	require.NoError(tb, err)
-
-	certFile, err := os.CreateTemp(testDirectory, "")
-	require.NoError(tb, err)
-	defer MustClose(tb, certFile)
-	tb.Cleanup(func() {
-		require.NoError(tb, os.Remove(certFile.Name()))
-	})
-
-	// create chained PEM file with CA and entity cert
-	for _, cert := range [][]byte{entityBytes, rootBytes} {
-		require.NoError(tb,
-			pem.Encode(certFile, &pem.Block{
-				Type:  "CERTIFICATE",
-				Bytes: cert,
-			}),
-		)
-	}
-
-	keyFile, err := os.CreateTemp(testDirectory, "")
-	require.NoError(tb, err)
-	defer MustClose(tb, keyFile)
-	tb.Cleanup(func() {
-		require.NoError(tb, os.Remove(keyFile.Name()))
-	})
-
-	entityKeyBytes, err := x509.MarshalECPrivateKey(entityKey)
-	require.NoError(tb, err)
-
-	require.NoError(tb,
-		pem.Encode(keyFile, &pem.Block{
-			Type:  "ECDSA PRIVATE KEY",
-			Bytes: entityKeyBytes,
-		}),
-	)
-
-	return certFile.Name(), keyFile.Name()
 }
 
 // GitalyOrPraefect returns either the Gitaly- or Praefect-specific object depending on whether
