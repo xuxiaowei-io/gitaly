0acc57c42192ab0fbaa4b465e068d13402b1773d
James Fargher <proglottis@gmail.com>
2023-06-02 05:57:58 +0000

    Applied 3 error tersification suggestions

--- internal/gitaly/service/operations/merge.go
+++ internal/gitaly/service/operations/merge.go
@@ -130,7 +130,7 @@ func (s *Server) UserMergeBranch(stream gitalypb.OperationService_UserMergeBranc
 			if errors.Is(err, git.ErrReferenceNotFound) {
 				return structerr.NewNotFound("%w", err)
 			}
-			return structerr.NewInternal("branch resolution failed: %w", err)
+			return structerr.NewInternal("branch resolution: %w", err)
 		}
 	}
 
@@ -170,7 +170,7 @@ func (s *Server) UserMergeBranch(stream gitalypb.OperationService_UserMergeBranc
 				)
 		}
 
-		return structerr.NewInternal("unknown merge error: %w", err)
+		return structerr.NewInternal("merge: %w", err)
 	}
 
 	mergeOID, err := git.ObjectHashSHA1.FromHex(mergeCommitID)
@@ -238,7 +238,7 @@ func (s *Server) UserMergeBranch(stream gitalypb.OperationService_UserMergeBranc
 			)
 		}
 
-		return structerr.NewInternal("target update failed: %w", err)
+		return structerr.NewInternal("target update: %w", err)
 	}
 
 	if err := stream.Send(&gitalypb.UserMergeBranchResponse{
