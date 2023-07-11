00b66c17cf58c4d3dd5bb0c8f32bccf85d1aed76
Patrick Steinhardt <psteinhardt@gitlab.com>
2023-07-11 12:06:04 +0200

    repository: Test CommitLanguages with SHA256
    Refactor the CommitLanguages tests to generate test data at runtime so
    that we can test with the SHA256 object format.

--- internal/gitaly/service/commit/languages_test.go
+++ internal/gitaly/service/commit/languages_test.go
@@ -1,134 +1,168 @@
-//go:build !gitaly_test_sha256
-
 package commit
 
 import (
 	"testing"
 
-	"github.com/stretchr/testify/require"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/git"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/git/gittest"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/gitaly/storage"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/structerr"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/testhelper"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/testhelper/testcfg"
 	"gitlab.com/gitlab-org/gitaly/v16/proto/go/gitalypb"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
-func TestLanguages(t *testing.T) {
+const (
+	rubyContent = "puts 'Hello world!'\n"
+
+	javaContent = `public class HelloWorldFactory {
+	public static void main(String[] args){
+		IHelloWorldFactory factory = HelloWorldFactory().getInstance();
+		IHelloWorld helloWorld = factory.getHelloWorld();
+		IHelloWorldString helloWorldString = helloWorld.getHelloWorldString();
+		IPrintStrategy printStrategy = helloWorld.getPrintStrategy();
+		helloWorld.print(printStrategy, helloWorldString%);
+	}
+}
+`
+
+	cContent = `#include <stdio.h>
+
+	int main(const char *argv[]) {
+		puts("Hello, world!")
+	}
+`
+)
+
+func TestCommitLanguages(t *testing.T) {
 	t.Parallel()
 
-	cfg := testcfg.Build(t)
 	ctx := testhelper.Context(t)
-
+	cfg := testcfg.Build(t)
 	cfg.SocketPath = startTestServices(t, cfg)
-
-	repo, _ := gittest.CreateRepository(t, ctx, cfg, gittest.CreateRepositoryConfig{
-		Seed: gittest.SeedGitLabTest,
-	})
-
 	client := newCommitServiceClient(t, cfg.SocketPath)
 
-	request := &gitalypb.CommitLanguagesRequest{
-		Repository: repo,
-		Revision:   []byte("cb19058ecc02d01f8e4290b7e79cafd16a8839b6"),
+	type setupData struct {
+		request          *gitalypb.CommitLanguagesRequest
+		expectedErr      error
+		expectedResponse *gitalypb.CommitLanguagesResponse
 	}
-
-	resp, err := client.CommitLanguages(ctx, request)
-	require.NoError(t, err)
-
-	expectedLanguages := []*gitalypb.CommitLanguagesResponse_Language{
-		{Name: "Ruby", Share: 65.28394, Color: "#701516", Bytes: 2943},
-		{Name: "JavaScript", Share: 22.493345, Color: "#f1e05a", Bytes: 1014},
-		{Name: "HTML", Share: 7.741792, Color: "#e34c26", Bytes: 349},
-		{Name: "CoffeeScript", Share: 2.373558, Color: "#244776", Bytes: 107},
-		{Name: "Modula-2", Share: 2.1073646, Color: "#10253f", Bytes: 95},
-	}
-
-	testhelper.ProtoEqual(t, expectedLanguages, resp.Languages)
-}
-
-func TestLanguagesEmptyRevision(t *testing.T) {
-	t.Parallel()
-
-	ctx := testhelper.Context(t)
-	_, repo, _, client := setupCommitServiceWithRepo(t, ctx)
-
-	request := &gitalypb.CommitLanguagesRequest{
-		Repository: repo,
-	}
-	resp, err := client.CommitLanguages(ctx, request)
-	require.NoError(t, err)
-
-	require.NotZero(t, len(resp.Languages), "number of languages in response")
-
-	foundRuby := false
-	for _, l := range resp.Languages {
-		if l.Name == "Ruby" {
-			foundRuby = true
-		}
-	}
-	require.True(t, foundRuby, "expected to find Ruby as a language on HEAD")
-}
-
-func TestInvalidCommitLanguagesRequestRevision(t *testing.T) {
-	t.Parallel()
-
-	ctx := testhelper.Context(t)
-	_, repo, _, client := setupCommitServiceWithRepo(t, ctx)
-
-	_, err := client.CommitLanguages(ctx, &gitalypb.CommitLanguagesRequest{
-		Repository: repo,
-		Revision:   []byte("--output=/meow"),
-	})
-	testhelper.RequireGrpcError(t, status.Error(codes.InvalidArgument, "revision can't start with '-'"), err)
-}
-
-func TestAmbiguousRefCommitLanguagesRequestRevision(t *testing.T) {
-	t.Parallel()
-
-	ctx := testhelper.Context(t)
-	_, repo, _, client := setupCommitServiceWithRepo(t, ctx)
-
-	// gitlab-test repo has both a branch and a tag named 'v1.1.0'
-	// b83d6e391c22777fca1ed3012fce84f633d7fed0 refs/heads/v1.1.0
-	// 8a2a6eb295bb170b34c24c76c49ed0e9b2eaf34b refs/tags/v1.1.0
-	_, err := client.CommitLanguages(ctx, &gitalypb.CommitLanguagesRequest{
-		Repository: repo,
-		Revision:   []byte("v1.1.0"),
-	})
-	require.NoError(t, err)
-}
-
-func TestCommitLanguages_validateRequest(t *testing.T) {
-	t.Parallel()
-
-	ctx := testhelper.Context(t)
-	_, repo, _, client := setupCommitServiceWithRepo(t, ctx)
 
 	for _, tc := range []struct {
-		desc        string
-		req         *gitalypb.CommitLanguagesRequest
-		expectedErr error
+		desc  string
+		setup func(t *testing.T) setupData
 	}{
 		{
-			desc:        "no repository provided",
-			req:         &gitalypb.CommitLanguagesRequest{Repository: nil},
-			expectedErr: structerr.NewInvalidArgument("%w", storage.ErrRepositoryNotSet),
+			desc: "no repository provided",
+			setup: func(t *testing.T) setupData {
+				return setupData{
+					request: &gitalypb.CommitLanguagesRequest{
+						Repository: nil,
+					},
+					expectedErr: structerr.NewInvalidArgument("%w", storage.ErrRepositoryNotSet),
+				}
+			},
 		},
 		{
-			desc: "invalid revision provided",
-			req: &gitalypb.CommitLanguagesRequest{
-				Repository: repo,
-				Revision:   []byte("invalid revision"),
+			desc: "invalid revision",
+			setup: func(t *testing.T) setupData {
+				repo, _ := gittest.CreateRepository(t, ctx, cfg)
+
+				return setupData{
+					request: &gitalypb.CommitLanguagesRequest{
+						Repository: repo,
+						Revision:   []byte("--output=/meow"),
+					},
+					expectedErr: structerr.NewInvalidArgument("revision can't start with '-'"),
+				}
 			},
-			expectedErr: status.Error(codes.InvalidArgument, "revision can't contain whitespace"),
+		},
+		{
+			desc: "empty revision falls back to default branch",
+			setup: func(t *testing.T) setupData {
+				repo, repoPath := gittest.CreateRepository(t, ctx, cfg)
+
+				gittest.WriteCommit(t, cfg, repoPath, gittest.WithBranch(git.DefaultBranch), gittest.WithTreeEntries(
+					gittest.TreeEntry{Path: "file.c", Mode: "100644", Content: cContent},
+				))
+
+				return setupData{
+					request: &gitalypb.CommitLanguagesRequest{
+						Repository: repo,
+						Revision:   nil,
+					},
+					expectedResponse: &gitalypb.CommitLanguagesResponse{
+						Languages: []*gitalypb.CommitLanguagesResponse_Language{
+							{Name: "C", Color: "#555555", Share: 100, Bytes: uint64(len(cContent))},
+						},
+					},
+				}
+			},
+		},
+		{
+			desc: "ambiguous revision",
+			setup: func(t *testing.T) setupData {
+				repo, repoPath := gittest.CreateRepository(t, ctx, cfg)
+
+				// Write both a branch and a tag named "v1.0.0" with different contents so that we can
+				// verify which of both references has precedence.
+				gittest.WriteCommit(t, cfg, repoPath, gittest.WithBranch("v1.0.0"), gittest.WithTreeEntries(
+					gittest.TreeEntry{Path: "file.c", Mode: "100644", Content: cContent},
+				))
+				gittest.WriteCommit(t, cfg, repoPath, gittest.WithReference("refs/tags/v1.0.0"), gittest.WithTreeEntries(
+					gittest.TreeEntry{Path: "file.rb", Mode: "100644", Content: rubyContent},
+				))
+
+				return setupData{
+					request: &gitalypb.CommitLanguagesRequest{
+						Repository: repo,
+						Revision:   []byte("v1.0.0"),
+					},
+					expectedResponse: &gitalypb.CommitLanguagesResponse{
+						Languages: []*gitalypb.CommitLanguagesResponse_Language{
+							{Name: "C", Color: "#555555", Share: 100, Bytes: uint64(len(cContent))},
+						},
+					},
+				}
+			},
+		},
+		{
+			desc: "multiple languages",
+			setup: func(t *testing.T) setupData {
+				repo, repoPath := gittest.CreateRepository(t, ctx, cfg)
+
+				commit := gittest.WriteCommit(t, cfg, repoPath, gittest.WithBranch(git.DefaultBranch), gittest.WithTreeEntries(
+					gittest.TreeEntry{Path: "file.rb", Mode: "100644", Content: rubyContent},
+					gittest.TreeEntry{Path: "file.java", Mode: "100644", Content: javaContent},
+					gittest.TreeEntry{Path: "file.c", Mode: "100644", Content: cContent},
+				))
+
+				return setupData{
+					request: &gitalypb.CommitLanguagesRequest{
+						Repository: repo,
+						Revision:   []byte(commit),
+					},
+					expectedResponse: &gitalypb.CommitLanguagesResponse{
+						Languages: []*gitalypb.CommitLanguagesResponse_Language{
+							{Name: "Java", Color: "#b07219", Share: 79.67145538330078, Bytes: uint64(len(javaContent))},
+							{Name: "C", Color: "#555555", Share: 16.221765518188477, Bytes: uint64(len(cContent))},
+							{Name: "Ruby", Color: "#701516", Share: 4.106776237487793, Bytes: uint64(len(rubyContent))},
+						},
+					},
+				}
+			},
 		},
 	} {
+		tc := tc
+
 		t.Run(tc.desc, func(t *testing.T) {
-			_, err := client.CommitLanguages(ctx, tc.req)
-			testhelper.RequireGrpcError(t, tc.expectedErr, err)
+			t.Parallel()
+
+			setup := tc.setup(t)
+
+			response, err := client.CommitLanguages(ctx, setup.request)
+			testhelper.RequireGrpcError(t, setup.expectedErr, err)
+			testhelper.ProtoEqual(t, setup.expectedResponse, response)
 		})
 	}
 }
