94cdd97baf4c1ebb03f7a15d0e6d7ac9799d48f6
Hordur Freyr Yngvason <hfyngvason@gitlab.com>
2023-07-11 15:19:03 +0000

    Add UserRebaseToRef RPC

--- internal/gitaly/service/operations/rebase.go
+++ internal/gitaly/service/operations/rebase.go
@@ -1,8 +1,10 @@
 package operations
 
 import (
+	"context"
 	"errors"
 	"fmt"
+	"strings"
 	"time"
 
 	"gitlab.com/gitlab-org/gitaly/v16/internal/git"
@@ -201,4 +203,135 @@ func (r rebaseRemoteFetch) GetStartRepository() *gitalypb.Repository {
 
 func (r rebaseRemoteFetch) GetStartBranchName() []byte {
 	return r.header.GetRemoteBranch()
+}
+
+func validateUserRebaseToRefRequest(locator storage.Locator, in *gitalypb.UserRebaseToRefRequest) error {
+	if err := locator.ValidateRepository(in.GetRepository()); err != nil {
+		return err
+	}
+
+	if len(in.FirstParentRef) == 0 {
+		return errors.New("empty FirstParentRef")
+	}
+
+	if in.User == nil {
+		return errors.New("empty User")
+	}
+
+	if in.SourceSha == "" {
+		return errors.New("empty SourceSha")
+	}
+
+	if len(in.TargetRef) == 0 {
+		return errors.New("empty TargetRef")
+	}
+
+	if !strings.HasPrefix(string(in.TargetRef), "refs/merge-requests") {
+		return errors.New("invalid TargetRef")
+	}
+
+	return nil
+}
+
+// UserRebaseToRef overwrites the given TargetRef with the result of rebasing
+// SourceSHA on top of FirstParentRef, and returns the SHA of the HEAD of
+// TargetRef.
+func (s *Server) UserRebaseToRef(ctx context.Context, request *gitalypb.UserRebaseToRefRequest) (*gitalypb.UserRebaseToRefResponse, error) {
+	if err := validateUserRebaseToRefRequest(s.locator, request); err != nil {
+		return nil, structerr.NewInvalidArgument("%w", err)
+	}
+
+	quarantineDir, quarantineRepo, err := s.quarantinedRepo(ctx, request.Repository)
+	if err != nil {
+		return nil, structerr.NewInternal("creating repo quarantine: %w", err)
+	}
+
+	repoPath, err := quarantineRepo.Path()
+	if err != nil {
+		return nil, err
+	}
+
+	oid, err := quarantineRepo.ResolveRevision(ctx, git.Revision(request.FirstParentRef))
+	if err != nil {
+		return nil, structerr.NewInvalidArgument("invalid FirstParentRef")
+	}
+
+	sourceOID, err := quarantineRepo.ResolveRevision(ctx, git.Revision(request.SourceSha))
+	if err != nil {
+		return nil, structerr.NewInvalidArgument("invalid SourceSha")
+	}
+
+	// Resolve the current state of the target reference. We do not care whether it
+	// exists or not, but what we do want to assert is that the target reference doesn't
+	// change while we compute the merge commit as a small protection against races.
+	objectHash, err := quarantineRepo.ObjectHash(ctx)
+	if err != nil {
+		return nil, structerr.NewInternal("detecting object hash: %w", err)
+	}
+
+	var oldTargetOID git.ObjectID
+	if expectedOldOID := request.GetExpectedOldOid(); expectedOldOID != "" {
+		oldTargetOID, err = objectHash.FromHex(expectedOldOID)
+		if err != nil {
+			return nil, structerr.NewInvalidArgument("invalid expected old object ID: %w", err).WithMetadata("old_object_id", expectedOldOID)
+		}
+
+		oldTargetOID, err = quarantineRepo.ResolveRevision(
+			ctx, git.Revision(fmt.Sprintf("%s^{object}", oldTargetOID)),
+		)
+		if err != nil {
+			return nil, structerr.NewInvalidArgument("cannot resolve expected old object ID: %w", err).
+				WithMetadata("old_object_id", expectedOldOID)
+		}
+	} else if targetRef, err := quarantineRepo.GetReference(ctx, git.ReferenceName(request.TargetRef)); err == nil {
+		if targetRef.IsSymbolic {
+			return nil, structerr.NewFailedPrecondition("target reference is symbolic: %q", request.TargetRef)
+		}
+
+		oid, err := objectHash.FromHex(targetRef.Target)
+		if err != nil {
+			return nil, structerr.NewInternal("invalid target revision: %w", err)
+		}
+
+		oldTargetOID = oid
+	} else if errors.Is(err, git.ErrReferenceNotFound) {
+		oldTargetOID = objectHash.ZeroOID
+	} else {
+		return nil, structerr.NewInternal("could not read target reference: %w", err)
+	}
+
+	committer := git2go.NewSignature(string(request.User.Name), string(request.User.Email), time.Now())
+	if request.Timestamp != nil {
+		committer.When = request.Timestamp.AsTime()
+	}
+
+	rebasedOID, err := s.git2goExecutor.Rebase(ctx, quarantineRepo, git2go.RebaseCommand{
+		Repository:       repoPath,
+		Committer:        committer,
+		CommitID:         sourceOID,
+		UpstreamCommitID: oid,
+		SkipEmptyCommits: true,
+	})
+	if err != nil {
+		var conflictErr git2go.ConflictingFilesError
+		if errors.As(err, &conflictErr) {
+			return nil, structerr.NewFailedPrecondition("failed to rebase %s on %s while preparing %s due to conflict",
+				sourceOID, oid, string(request.TargetRef))
+		}
+
+		return nil, structerr.NewInternal("rebasing commits: %w", err)
+	}
+
+	if err := quarantineDir.Migrate(); err != nil {
+		return nil, structerr.NewInternal("migrating quarantined objects: %w", err)
+	}
+
+	repo := s.localrepo(request.GetRepository())
+	if err := repo.UpdateRef(ctx, git.ReferenceName(request.TargetRef), rebasedOID, oldTargetOID); err != nil {
+		return nil, structerr.NewFailedPrecondition("could not update %s. Please refresh and try again", string(request.TargetRef))
+	}
+
+	return &gitalypb.UserRebaseToRefResponse{
+		CommitId: rebasedOID.String(),
+	}, nil
 }
