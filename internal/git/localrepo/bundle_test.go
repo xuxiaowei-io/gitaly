64646f1d3940347b13f1bb36f2b4c27930219be6
Justin Tobler <jtobler@gitlab.com>
2023-07-25 15:06:13 -0500

    localrepo: Introduce `localrepo.CloneBundle` method
    When creating a repository from a bundle, it is necessary to detect the
    object format of the repository to ensure the correct repository is
    initialized. To facilitate this, the `localrepo.CloneBundle` method is
    introduced which performs a `git-clone(1)` on a Git bundle, thus
    automatically initializing the repository with the same object format
    used by the bundle.

--- internal/git/localrepo/bundle_test.go
+++ internal/git/localrepo/bundle_test.go
@@ -13,10 +13,14 @@ import (
 
 	"github.com/stretchr/testify/require"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/git"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/git/catfile"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/git/gittest"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/gitaly/config"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/gitaly/transaction"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/structerr"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/testhelper"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/testhelper/testcfg"
+	"gitlab.com/gitlab-org/gitaly/v16/proto/go/gitalypb"
 )
 
 func TestRepoCreateBundle(t *testing.T) {
@@ -111,6 +115,126 @@ func TestRepoCreateBundle(t *testing.T) {
 			}
 
 			require.Equal(t, tc.expectedRefs, refNames)
+		})
+	}
+}
+
+func TestRepo_CloneBundle(t *testing.T) {
+	t.Parallel()
+
+	createBundle := func(tb testing.TB, cfg config.Cfg, repoPath string) io.Reader {
+		tmp := testhelper.TempDir(tb)
+		bundlePath := filepath.Join(tmp, "test.bundle")
+		gittest.BundleRepo(t, cfg, repoPath, bundlePath)
+
+		bundle, err := os.Open(bundlePath)
+		require.NoError(t, err)
+		t.Cleanup(func() { testhelper.MustClose(t, bundle) })
+
+		return bundle
+	}
+
+	type setupData struct {
+		reader          io.Reader
+		expectedHeadRef git.ReferenceName
+		expectedRefs    []git.Reference
+		expectedErr     error
+	}
+
+	for _, tc := range []struct {
+		desc  string
+		setup func(t *testing.T, ctx context.Context, cfg config.Cfg) setupData
+	}{
+		{
+			desc: "clone from Git bundle",
+			setup: func(t *testing.T, ctx context.Context, cfg config.Cfg) setupData {
+				// Create a repository used to generate a Git bundle from.
+				_, bundleRepoPath := gittest.CreateRepository(t, ctx, cfg, gittest.CreateRepositoryConfig{
+					SkipCreationViaService: true,
+				})
+
+				// Write objects to the repository so the contents of the extracted bundle can be
+				// validated. Also change the repository HEAD to validate that the cloned repository
+				// HEAD is also updated.
+				mainOID := gittest.WriteCommit(t, cfg, bundleRepoPath, gittest.WithBranch(git.DefaultBranch))
+				featureOID := gittest.WriteCommit(t, cfg, bundleRepoPath, gittest.WithBranch("feature"), gittest.WithParents(mainOID))
+				gittest.Exec(t, cfg, "-C", bundleRepoPath, "symbolic-ref", "HEAD", "refs/heads/feature")
+
+				return setupData{
+					reader:          createBundle(t, cfg, bundleRepoPath),
+					expectedHeadRef: git.NewReferenceNameFromBranchName("feature"),
+					expectedRefs: []git.Reference{
+						git.NewReference(git.NewReferenceNameFromBranchName("feature"), featureOID),
+						git.NewReference(git.DefaultRef, mainOID),
+					},
+				}
+			},
+		},
+		{
+			desc: "empty bundle",
+			setup: func(t *testing.T, ctx context.Context, cfg config.Cfg) setupData {
+				// If the Git bundle is empty, the repository will fail to be cloned.
+				return setupData{
+					reader:      new(bytes.Reader),
+					expectedErr: structerr.New("waiting for git-clone: exit status 128"),
+				}
+			},
+		},
+		{
+			desc: "invalid bundle",
+			setup: func(t *testing.T, ctx context.Context, cfg config.Cfg) setupData {
+				// If the Git bundle is invalid, the repository will fail to be cloned.
+				return setupData{
+					reader:      strings.NewReader("invalid bundle"),
+					expectedErr: structerr.New("waiting for git-clone: exit status 128"),
+				}
+			},
+		},
+	} {
+		tc := tc
+		t.Run(tc.desc, func(t *testing.T) {
+			t.Parallel()
+
+			ctx := testhelper.Context(t)
+			cfg := testcfg.Build(t)
+			gitCmdFactory := gittest.NewCommandFactory(t, cfg)
+			catfileCache := catfile.NewCache(cfg)
+			defer catfileCache.Stop()
+
+			repoProto := &gitalypb.Repository{
+				StorageName:  cfg.Storages[0].Name,
+				RelativePath: gittest.NewRepositoryName(t),
+			}
+
+			data := tc.setup(t, ctx, cfg)
+			repo := New(config.NewLocator(cfg), gitCmdFactory, catfileCache, repoProto)
+
+			err := repo.CloneBundle(ctx, data.reader)
+			if data.expectedErr != nil {
+				require.ErrorContains(t, err, data.expectedErr.Error())
+				return
+			}
+			require.NoError(t, err)
+
+			repoPath, err := repo.Path()
+			require.NoError(t, err)
+
+			// Verify connectivity and validity of the repository objects.
+			gittest.Exec(t, cfg, "-C", repoPath, "fsck")
+
+			// Verify HEAD has been set correctly.
+			headRef, err := repo.HeadReference(ctx)
+			require.NoError(t, err)
+			require.Equal(t, data.expectedHeadRef, headRef)
+
+			// Verify the repository has the correct references.
+			refs, err := repo.GetReferences(ctx)
+			require.NoError(t, err)
+			require.Equal(t, data.expectedRefs, refs)
+
+			// Verify the repository does not have remote set.
+			remotes := gittest.Exec(t, cfg, "-C", repoPath, "remote")
+			require.Empty(t, remotes)
 		})
 	}
 }
