ad400e7ef734158336f3abb82059d3a7e447def7
Karthik Nayak <knayak@gitlab.com>
2023-08-11 10:30:07 +0200

    featureflag: Remove the `CatfileBatchCommand` flag
    The `CatfileBatchCommand` flag was used to rollout the `--batch-command`
    option of `git-cat-file(1)`. Since the flag has been enabled for a while
    now, we can delete the flag and default to the enabled state.
    
    We cannot remove the old code because we are still dependent on the
    minimum git version supporting `--batch-command`, which is scheduled to
    land in git 2.42.0.

--- internal/testhelper/testhelper.go
+++ internal/testhelper/testhelper.go
@@ -202,9 +202,6 @@ func ContextWithoutCancel(opts ...ContextOpt) context.Context {
 	// deep in the call stack, so almost every test function would have to inject it into its
 	// context. The values of these flags should be randomized to increase the test coverage.
 	ctx = featureflag.ContextWithFeatureFlag(ctx, featureflag.RunCommandsInCGroup, true)
-	// CatfileBatchCommand affects many tests since most of them rely on catfile for content/info
-	// information about objects.
-	ctx = featureflag.ContextWithFeatureFlag(ctx, featureflag.CatfileBatchCommand, rand.Int()%2 == 0)
 	// Randomly enable mailmap
 	ctx = featureflag.ContextWithFeatureFlag(ctx, featureflag.MailmapOptions, rand.Int()%2 == 0)
 	// LowerBigFileThreshold is checked on every spawn of Git commands and is thus infeasible to be checked for
