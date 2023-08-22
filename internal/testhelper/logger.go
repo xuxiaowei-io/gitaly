a3dedd2661cada20c703b79d93c6fbbb62d48f90
Sami Hiltunen <shiltunen@gitlab.com>
2023-08-22 14:01:06 +0300

    Implement a method for test case shared loggers
    We were previously discarding logs in most of the test cases. As
    such, the loggers are not being passed around in the test cases
    and multiple loggers are instantiated. The log messages printed
    to multiple loggers are unordered which makes it more difficult
    to see the exact output of the logs. As a workaround, this commit
    implements a sharing mechanism to ensure all logs in a given test
    case end up in the same logger. The first call to the SharedLogger
    method creates a new logger, and the follow up calls will use the
    same logger within the same test case.
    
    The function will be wired in a follow up commit.

--- internal/testhelper/logger.go
+++ internal/testhelper/logger.go
@@ -4,12 +4,53 @@ import (
 	"bytes"
 	"os"
 	"path/filepath"
+	"sync"
 	"testing"
 
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/require"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/helper/perm"
 )
+
+var (
+	// sharedLoggersMutex protects the sharedLoggers map below.
+	sharedLoggersMutex sync.Mutex
+	// sharedLogger contains test case specific loggers keyed by the test name.
+	// sharedLoggersMutex should be acquired before accessing the map.
+	sharedLoggers = map[string]*logrus.Logger{}
+)
+
+// SharedLogger returns a logger that is global to the running test case.
+// When called first time during a test, a new logger is created and
+// returned. All follow up calls to SharedLogger return the same logger
+// instance.
+//
+// This is more of a workaround. It would be preferable to inject the
+// same logger returned from the first call everywhere in the test. We
+// have however a large number of tests which are creating new loggers
+// all over the place instead of passing the logger around. This sharing
+// mechanism serves as a workaround to use the same logger everywhere in
+// the same test case. Using the same logger ensures the log messages
+// are properly ordered.
+func SharedLogger(tb testing.TB) *logrus.Logger {
+	sharedLoggersMutex.Lock()
+	defer sharedLoggersMutex.Unlock()
+
+	if logger, ok := sharedLoggers[tb.Name()]; ok {
+		return logger
+	}
+
+	logger := NewLogger(tb, WithLoggerName("shared-logger"))
+	sharedLoggers[tb.Name()] = logger
+
+	tb.Cleanup(func() {
+		sharedLoggersMutex.Lock()
+		delete(sharedLoggers, tb.Name())
+		sharedLoggersMutex.Unlock()
+	})
+
+	return logger
+}
 
 type loggerOptions struct {
 	name string
