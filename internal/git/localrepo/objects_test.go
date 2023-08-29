cd18b867ebdf7588591bf781954eb5dd8ddfa875
Patrick Steinhardt <psteinhardt@gitlab.com>
2023-08-29 14:46:04 +0200

    localrepo: Split functions to write blobs into a standalone file
    The "objects.go" file is a grab bag of object-related functionality.
    It's not exactly clear to figure out what funcitonality is implemented
    in that file versus in an object-type specific standalone file though,
    mostly because we also have "commit.go".
    
    Fix this by splitting the file apart into generic object related
    functions and per-type functions, where this commit moves blob related
    functionality over.

--- internal/git/localrepo/objects_test.go
+++ internal/git/localrepo/objects_test.go
@@ -3,94 +3,21 @@ package localrepo
 import (
 	"bytes"
 	"fmt"
-	"io"
-	"os"
-	"path/filepath"
 	"strings"
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/git"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/git/catfile"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/git/gittest"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/gitaly/config"
-	"gitlab.com/gitlab-org/gitaly/v16/internal/helper/perm"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/helper/text"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/testhelper"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/testhelper/testcfg"
 	"gitlab.com/gitlab-org/gitaly/v16/proto/go/gitalypb"
 	"google.golang.org/grpc/metadata"
 )
-
-type ReaderFunc func([]byte) (int, error)
-
-func (fn ReaderFunc) Read(b []byte) (int, error) { return fn(b) }
-
-func TestRepo_WriteBlob(t *testing.T) {
-	t.Parallel()
-
-	ctx := testhelper.Context(t)
-	_, repo, repoPath := setupRepo(t)
-
-	for _, tc := range []struct {
-		desc       string
-		attributes string
-		input      io.Reader
-		sha        string
-		error      error
-		content    string
-	}{
-		{
-			desc:  "error reading",
-			input: ReaderFunc(func([]byte) (int, error) { return 0, assert.AnError }),
-			error: assert.AnError,
-		},
-		{
-			desc:    "successful empty blob",
-			input:   strings.NewReader(""),
-			content: "",
-		},
-		{
-			desc:    "successful blob",
-			input:   strings.NewReader("some content"),
-			content: "some content",
-		},
-		{
-			desc:    "LF line endings left unmodified",
-			input:   strings.NewReader("\n"),
-			content: "\n",
-		},
-		{
-			desc:    "CRLF converted to LF due to global git config",
-			input:   strings.NewReader("\r\n"),
-			content: "\n",
-		},
-		{
-			desc:       "line endings preserved in binary files",
-			input:      strings.NewReader("\r\n"),
-			attributes: "file-path binary",
-			content:    "\r\n",
-		},
-	} {
-		t.Run(tc.desc, func(t *testing.T) {
-			attributesPath := filepath.Join(repoPath, "info", "attributes")
-			require.NoError(t, os.MkdirAll(filepath.Dir(attributesPath), perm.SharedDir))
-			require.NoError(t, os.WriteFile(attributesPath, []byte(tc.attributes), perm.PublicFile))
-
-			sha, err := repo.WriteBlob(ctx, "file-path", tc.input)
-			require.Equal(t, tc.error, err)
-			if tc.error != nil {
-				return
-			}
-
-			content, err := repo.ReadObject(ctx, sha)
-			require.NoError(t, err)
-			assert.Equal(t, tc.content, string(content))
-		})
-	}
-}
 
 func TestFormatTag(t *testing.T) {
 	t.Parallel()
