bdc4ec5f9ee72ce6291b72cd181528a7ed8dc62d
Patrick Steinhardt <psteinhardt@gitlab.com>
2023-08-30 12:36:34 +0200

    operations: Always use Git implementation of UserRebaseToRef
    in a7c909970 (operations: Implement UserRebaseToRef via plain Git,
    2023-08-18), we have reimplemented UserRebaseToRef with pure Git
    commands, which allows us to get rid of libgit2. We have since rolled
    out this change to production systems without any observed issues.
    
    Remove the feature flag so that we always use the new implementation
    that doesn't require libgit2 anymore.

--- internal/gitaly/service/operations/rebase_to_ref.go
+++ internal/gitaly/service/operations/rebase_to_ref.go
@@ -6,10 +6,8 @@ import (
 	"fmt"
 	"time"
 
-	"gitlab.com/gitlab-org/gitaly/v16/internal/featureflag"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/git"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/git/localrepo"
-	"gitlab.com/gitlab-org/gitaly/v16/internal/git2go"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/structerr"
 	"gitlab.com/gitlab-org/gitaly/v16/proto/go/gitalypb"
 )
@@ -25,11 +23,6 @@ func (s *Server) UserRebaseToRef(ctx context.Context, request *gitalypb.UserReba
 	quarantineDir, quarantineRepo, err := s.quarantinedRepo(ctx, request.Repository)
 	if err != nil {
 		return nil, structerr.NewInternal("creating repo quarantine: %w", err)
-	}
-
-	repoPath, err := quarantineRepo.Path()
-	if err != nil {
-		return nil, err
 	}
 
 	oid, err := quarantineRepo.ResolveRevision(ctx, git.Revision(request.FirstParentRef))
@@ -86,40 +79,20 @@ func (s *Server) UserRebaseToRef(ctx context.Context, request *gitalypb.UserReba
 		committer.When = request.Timestamp.AsTime()
 	}
 
-	var rebasedOID git.ObjectID
-	if featureflag.UserRebaseToRefPureGit.IsEnabled(ctx) {
-		rebasedOID, err = quarantineRepo.Rebase(
-			ctx,
-			oid.String(),
-			sourceOID.String(),
-			localrepo.RebaseWithCommitter(committer),
-		)
-		if err != nil {
-			var conflictErr *localrepo.RebaseConflictError
-			if errors.As(err, &conflictErr) {
-				return nil, structerr.NewFailedPrecondition("failed to rebase %s on %s while preparing %s due to conflict",
-					sourceOID, oid, string(request.TargetRef))
-			}
-
-			return nil, structerr.NewInternal("rebasing commits: %w", err)
+	rebasedOID, err := quarantineRepo.Rebase(
+		ctx,
+		oid.String(),
+		sourceOID.String(),
+		localrepo.RebaseWithCommitter(committer),
+	)
+	if err != nil {
+		var conflictErr *localrepo.RebaseConflictError
+		if errors.As(err, &conflictErr) {
+			return nil, structerr.NewFailedPrecondition("failed to rebase %s on %s while preparing %s due to conflict",
+				sourceOID, oid, string(request.TargetRef))
 		}
-	} else {
-		rebasedOID, err = s.git2goExecutor.Rebase(ctx, quarantineRepo, git2go.RebaseCommand{
-			Repository:       repoPath,
-			Committer:        committer,
-			CommitID:         sourceOID,
-			UpstreamCommitID: oid,
-			SkipEmptyCommits: true,
-		})
-		if err != nil {
-			var conflictErr git2go.ConflictingFilesError
-			if errors.As(err, &conflictErr) {
-				return nil, structerr.NewFailedPrecondition("failed to rebase %s on %s while preparing %s due to conflict",
-					sourceOID, oid, string(request.TargetRef))
-			}
 
-			return nil, structerr.NewInternal("rebasing commits: %w", err)
-		}
+		return nil, structerr.NewInternal("rebasing commits: %w", err)
 	}
 
 	if err := quarantineDir.Migrate(); err != nil {
