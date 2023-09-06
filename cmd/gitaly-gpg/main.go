124401a6ef9f9c062b01a77d044adde9c53801c6
John McDonnell <jmcdonnell@gitlab.com>
2023-09-06 12:04:49 +0100

    Move doc comment relating to gpg-signing to relevant binary

--- cmd/gitaly-gpg/main.go
+++ cmd/gitaly-gpg/main.go
@@ -57,6 +57,15 @@ func gpgApp() *cli.App {
 	}
 }
 
+// This binary is used to support signing through providing the path to the
+// actual signing key. Git itself does not support this since it simply calls
+// out to gpg(1), which only supports fetching keys from the gpg database.
+// This binary is used as a stopgap measure since we can set Git's gpg.program
+// config to point to this binary, which interprets the key_id passed in as the
+// path to the signing key.
+// In the future, we will modify Git so that commit-tree can take in a raw
+// commit message that we can add a signature to, at which point we can sign
+// commits manually and get rid of this binary.
 func main() {
 	if err := gpgApp().Run(os.Args); err != nil {
 		log.Fatal(err)
