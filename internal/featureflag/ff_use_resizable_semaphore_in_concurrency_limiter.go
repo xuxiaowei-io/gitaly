89ceed0775da605114479b289ef57c397327ed22
Quang-Minh Nguyen <qmnguyen@gitlab.com>
2023-09-26 15:18:14 +0700

    Enable use_resizable_semaphore_in_concurrency_limiter flag by default
    This flag controls the usage of the resizble semaphore implementation.
    This semaphore is the core of the concurrency limiter. This commit
    enables the flag by default after it is enabled on GitLab.com
    production.
    - MR: https://gitlab.com/gitlab-org/gitaly/-/merge_requests/6366
    - Rollout issue: https://gitlab.com/gitlab-org/gitaly/-/issues/5581

--- internal/featureflag/ff_use_resizable_semaphore_in_concurrency_limiter.go
+++ internal/featureflag/ff_use_resizable_semaphore_in_concurrency_limiter.go
@@ -8,5 +8,5 @@ var UseResizableSemaphoreInConcurrencyLimiter = NewFeatureFlag(
 	"use_resizable_semaphore_in_concurrency_limiter",
 	"v16.5.0",
 	"https://gitlab.com/gitlab-org/gitaly/-/issues/5581",
-	false,
+	true,
 )
