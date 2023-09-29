77fc600db42a271735b7f89cf3c573b1ec91fbf8
Patrick Steinhardt <psteinhardt@gitlab.com>
2023-09-29 09:39:23 +0200

    sentryhandler: Add tests for interceptors
    We don't have any tests for either the Unary nor for the Stream
    interceptor for our sentryhandler. Add some to verify that they behave
    as expected.

--- internal/grpc/middleware/sentryhandler/sentryhandler_test.go
+++ internal/grpc/middleware/sentryhandler/sentryhandler_test.go
@@ -3,15 +3,21 @@ package sentryhandler
 import (
 	"context"
 	"fmt"
+	"net"
 	"testing"
 	"time"
 
 	sentry "github.com/getsentry/sentry-go"
 	grpcmwtags "github.com/grpc-ecosystem/go-grpc-middleware/tags"
 	"github.com/stretchr/testify/require"
+	"gitlab.com/gitlab-org/gitaly/v16/internal/structerr"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/testhelper"
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/interop/grpc_testing"
 	"google.golang.org/grpc/status"
+	"google.golang.org/grpc/test/bufconn"
 )
 
 func TestGenerateSentryEvent(t *testing.T) {
@@ -183,4 +189,269 @@ func TestGenerateSentryEvent(t *testing.T) {
 			require.Equal(t, tc.expectedEvent, event)
 		})
 	}
+}
+
+func TestUnaryLogHandler(t *testing.T) {
+	t.Parallel()
+
+	ctx := testhelper.Context(t)
+
+	for _, tc := range []struct {
+		desc           string
+		handler        func(context.Context, *grpc_testing.SimpleRequest) (*grpc_testing.SimpleResponse, error)
+		expectedEvents []*sentry.Event
+	}{
+		{
+			desc: "no error",
+			handler: func(context.Context, *grpc_testing.SimpleRequest) (*grpc_testing.SimpleResponse, error) {
+				return &grpc_testing.SimpleResponse{}, nil
+			},
+		},
+		{
+			desc: "with error",
+			handler: func(context.Context, *grpc_testing.SimpleRequest) (*grpc_testing.SimpleResponse, error) {
+				return nil, fmt.Errorf("oopsie")
+			},
+			expectedEvents: []*sentry.Event{
+				{
+					Message:     "oopsie",
+					Transaction: "::grpc.testing.TestService/UnaryCall",
+					Tags: map[string]string{
+						"grpc.code":    "Unknown",
+						"grpc.method":  "/grpc.testing.TestService/UnaryCall",
+						"peer.address": "bufconn",
+						"system":       "grpc",
+					},
+					Fingerprint: []string{
+						"grpc", "::grpc.testing.TestService/UnaryCall", "Unknown",
+					},
+					Exception: []sentry.Exception{
+						{
+							Type:  "*errors.errorString",
+							Value: "oopsie",
+						},
+					},
+					Extra:    map[string]any{},
+					Contexts: map[string]map[string]any{},
+					Modules:  map[string]string{},
+				},
+			},
+		},
+		{
+			desc: "with structured error",
+			handler: func(context.Context, *grpc_testing.SimpleRequest) (*grpc_testing.SimpleResponse, error) {
+				return nil, structerr.NewNotFound("not found")
+			},
+			expectedEvents: []*sentry.Event{
+				{
+					Message:     "not found",
+					Transaction: "::grpc.testing.TestService/UnaryCall",
+					Tags: map[string]string{
+						"grpc.code":    "NotFound",
+						"grpc.method":  "/grpc.testing.TestService/UnaryCall",
+						"peer.address": "bufconn",
+						"system":       "grpc",
+					},
+					Fingerprint: []string{
+						"grpc", "::grpc.testing.TestService/UnaryCall", "NotFound",
+					},
+					Exception: []sentry.Exception{
+						{
+							Type:  "structerr.Error",
+							Value: "not found",
+						},
+					},
+					Extra:    map[string]any{},
+					Contexts: map[string]map[string]any{},
+					Modules:  map[string]string{},
+				},
+			},
+		},
+		{
+			desc: "with error marked to skip",
+			handler: func(ctx context.Context, _ *grpc_testing.SimpleRequest) (*grpc_testing.SimpleResponse, error) {
+				MarkToSkip(ctx)
+				return nil, fmt.Errorf("this error should be skipped")
+			},
+			expectedEvents: nil,
+		},
+	} {
+		t.Run(tc.desc, func(t *testing.T) {
+			server := mockServiceServer{
+				unaryCallHandler: tc.handler,
+			}
+			client := server.setup(t, ctx)
+
+			// We don't care about any errors returned by this call.
+			_, _ = client.UnaryCall(ctx, &grpc_testing.SimpleRequest{})
+
+			// The exact time spent in the RPC is indeterministic, so we only verify that the field exists
+			// without asserting its exact value.
+			for _, event := range server.events {
+				require.Contains(t, event.Tags, "grpc.time_ms")
+				delete(event.Tags, "grpc.time_ms")
+			}
+
+			require.Equal(t, tc.expectedEvents, server.events)
+		})
+	}
+}
+
+func TestStreamLogHandler(t *testing.T) {
+	t.Parallel()
+
+	ctx := testhelper.Context(t)
+
+	for _, tc := range []struct {
+		desc           string
+		handler        func(server grpc_testing.TestService_FullDuplexCallServer) error
+		expectedEvents []*sentry.Event
+	}{
+		{
+			desc: "no error",
+			handler: func(server grpc_testing.TestService_FullDuplexCallServer) error {
+				return nil
+			},
+		},
+		{
+			desc: "with error",
+			handler: func(server grpc_testing.TestService_FullDuplexCallServer) error {
+				return fmt.Errorf("oopsie")
+			},
+			expectedEvents: []*sentry.Event{
+				{
+					Message:     "oopsie",
+					Transaction: "::grpc.testing.TestService/FullDuplexCall",
+					Tags: map[string]string{
+						"grpc.code":    "Unknown",
+						"grpc.method":  "/grpc.testing.TestService/FullDuplexCall",
+						"peer.address": "bufconn",
+						"system":       "grpc",
+					},
+					Fingerprint: []string{
+						"grpc", "::grpc.testing.TestService/FullDuplexCall", "Unknown",
+					},
+					Exception: []sentry.Exception{
+						{
+							Type:  "*errors.errorString",
+							Value: "oopsie",
+						},
+					},
+					Extra:    map[string]any{},
+					Contexts: map[string]map[string]any{},
+					Modules:  map[string]string{},
+				},
+			},
+		},
+		{
+			desc: "with structured error",
+			handler: func(server grpc_testing.TestService_FullDuplexCallServer) error {
+				return structerr.NewNotFound("not found")
+			},
+			expectedEvents: []*sentry.Event{
+				{
+					Message:     "not found",
+					Transaction: "::grpc.testing.TestService/FullDuplexCall",
+					Tags: map[string]string{
+						"grpc.code":    "NotFound",
+						"grpc.method":  "/grpc.testing.TestService/FullDuplexCall",
+						"peer.address": "bufconn",
+						"system":       "grpc",
+					},
+					Fingerprint: []string{
+						"grpc", "::grpc.testing.TestService/FullDuplexCall", "NotFound",
+					},
+					Exception: []sentry.Exception{
+						{
+							Type:  "structerr.Error",
+							Value: "not found",
+						},
+					},
+					Extra:    map[string]any{},
+					Contexts: map[string]map[string]any{},
+					Modules:  map[string]string{},
+				},
+			},
+		},
+		{
+			desc: "with error marked to skip",
+			handler: func(server grpc_testing.TestService_FullDuplexCallServer) error {
+				MarkToSkip(server.Context())
+				return fmt.Errorf("this error should be skipped")
+			},
+			expectedEvents: nil,
+		},
+	} {
+		t.Run(tc.desc, func(t *testing.T) {
+			server := mockServiceServer{
+				fullDuplexCallHandler: tc.handler,
+			}
+			client := server.setup(t, ctx)
+
+			stream, err := client.FullDuplexCall(ctx)
+			require.NoError(t, err)
+			// We don't care about any errors returned by this call.
+			_, _ = stream.Recv()
+
+			// The exact time spent in the RPC is indeterministic, so we only verify that the field exists
+			// without asserting its exact value.
+			for _, event := range server.events {
+				require.Contains(t, event.Tags, "grpc.time_ms")
+				delete(event.Tags, "grpc.time_ms")
+			}
+
+			require.Equal(t, tc.expectedEvents, server.events)
+		})
+	}
+}
+
+type mockServiceServer struct {
+	grpc_testing.TestServiceServer
+	unaryCallHandler      func(context.Context, *grpc_testing.SimpleRequest) (*grpc_testing.SimpleResponse, error)
+	fullDuplexCallHandler func(server grpc_testing.TestService_FullDuplexCallServer) error
+	events                []*sentry.Event
+}
+
+func (s *mockServiceServer) setup(tb testing.TB, ctx context.Context) grpc_testing.TestServiceClient {
+	tb.Helper()
+
+	eventReporterOption := WithEventReporter(func(event *sentry.Event) *sentry.EventID {
+		s.events = append(s.events, event)
+		return nil
+	})
+
+	server := grpc.NewServer(
+		grpc.ChainUnaryInterceptor(
+			grpcmwtags.UnaryServerInterceptor(),
+			UnaryLogHandler(eventReporterOption),
+		),
+		grpc.ChainStreamInterceptor(
+			grpcmwtags.StreamServerInterceptor(),
+			StreamLogHandler(eventReporterOption),
+		),
+	)
+	tb.Cleanup(server.Stop)
+	grpc_testing.RegisterTestServiceServer(server, s)
+
+	listener := bufconn.Listen(1)
+	go testhelper.MustServe(tb, server, listener)
+
+	conn, err := grpc.DialContext(ctx, listener.Addr().String(),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithContextDialer(func(ctx context.Context, _ string) (net.Conn, error) {
+			return listener.DialContext(ctx)
+		}),
+	)
+	require.NoError(tb, err)
+	tb.Cleanup(func() { testhelper.MustClose(tb, conn) })
+
+	return grpc_testing.NewTestServiceClient(conn)
+}
+
+func (s mockServiceServer) UnaryCall(ctx context.Context, request *grpc_testing.SimpleRequest) (*grpc_testing.SimpleResponse, error) {
+	return s.unaryCallHandler(ctx, request)
+}
+
+func (s mockServiceServer) FullDuplexCall(server grpc_testing.TestService_FullDuplexCallServer) error {
+	return s.fullDuplexCallHandler(server)
 }
