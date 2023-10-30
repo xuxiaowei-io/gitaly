b064ca00f45afd4584d3b145e510ab34ed2a3266
Sami Hiltunen <shiltunen@gitlab.com>
2023-10-30 20:16:20 +0200

    Enable transactions for GetObjectPool
    This commit enables transactions for GetObjectPool. With transactions,
    the RPC is targeting the snapshot. The RPC handler is thus updated
    to return the original relative path of the object pool by stripping
    the snapshot prefix from the returned relative path.

--- internal/gitaly/storage/storagemgr/middleware.go
+++ internal/gitaly/storage/storagemgr/middleware.go
@@ -46,7 +46,6 @@ var NonTransactionalRPCs = map[string]struct{}{
 	"/gitaly.ObjectPoolService/LinkRepositoryToObjectPool": {},
 	"/gitaly.ObjectPoolService/DisconnectGitAlternates":    {},
 	"/gitaly.ObjectPoolService/FetchIntoObjectPool":        {},
-	"/gitaly.ObjectPoolService/GetObjectPool":              {},
 	// GetSnapshot is testing logic with object pools as well.
 	"/gitaly.RepositoryService/GetSnapshot": {},
 	// CreateFork relies on object pools.
