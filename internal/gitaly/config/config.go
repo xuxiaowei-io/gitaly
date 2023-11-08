c6835226e2b1cf16907139c7d4c78213b15177d6
Quang-Minh Nguyen <qmnguyen@gitlab.com>
2023-11-08 17:27:15 +0700

    Set logging level config to "info" explicitly
    In multiple places, we state that the default log level is "info" by
    default. The "info" severity is set when the underlying logging library
    parses the log level unsuccessfully. We don't have set the default
    config to "info".
    
    In a prior commit, we fixed a grpc logger issue due to the missing of
    configured log level. To prevent that from happening again at another
    place, this commit sets a default logging level.

--- internal/gitaly/config/config.go
+++ internal/gitaly/config/config.go
@@ -576,6 +576,14 @@ func (scc StreamCacheConfig) Validate() error {
 		AsError()
 }
 
+func defaultLoggingConfig() Logging {
+	return Logging{
+		Config: log.Config{
+			Level: "info",
+		},
+	}
+}
+
 func defaultPackObjectsCacheConfig() StreamCacheConfig {
 	return StreamCacheConfig{
 		// The Pack-Objects cache is effective at deduplicating concurrent
@@ -608,6 +616,7 @@ func defaultPackObjectsLimiting() PackObjectsLimiting {
 func Load(file io.Reader) (Cfg, error) {
 	cfg := Cfg{
 		Prometheus:          prometheus.DefaultConfig(),
+		Logging:             defaultLoggingConfig(),
 		PackObjectsCache:    defaultPackObjectsCacheConfig(),
 		PackObjectsLimiting: defaultPackObjectsLimiting(),
 	}
