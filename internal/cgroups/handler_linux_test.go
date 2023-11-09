d7d074c8800fd1ec26af2c06c0cc0dbb332d18f6
Will Chandler <wchandler@gitlab.com>
2023-11-08 22:07:45 -0500

    cgroups: Unify TestSetup_ParentCgroups
    Coalesce `TestSetup_ParentCgroups()` and `TestSetup_ParentCgroupsV2()`
    into a single test that handles both v1 and v2 cgroups.
    
    Note that the `requireCgroupComponents()` helper introduced slightly
    changes the conditions checked versus the v1 test. The v2 manager
    requires that `setupMockCgroupFiles()` be executed before running
    `Setup()`. The v1 test used `requireCgroup()`, which would verify that a
    file was _not_ present, which is no longer be the case now that we have
    setup the cgroup files. We switch to `requireCgroupWithInt()` to avoid
    this failure.

--- internal/cgroups/handler_linux_test.go
+++ internal/cgroups/handler_linux_test.go
@@ -3,6 +3,9 @@
 package cgroups
 
 import (
+	"fmt"
+	"path/filepath"
+	"strconv"
 	"testing"
 
 	cgrps "github.com/containerd/cgroups/v3"
@@ -22,4 +25,140 @@ func TestNewManager(t *testing.T) {
 	require.IsType(t, &cgroupV2Handler{}, manager.handler)
 	manager = newCgroupManagerWithMode(cfg, testhelper.SharedLogger(t), 1, cgrps.Unavailable)
 	require.Nil(t, manager)
+}
+
+type expectedCgroup struct {
+	wantMemoryBytes int
+	wantCPUShares   int
+	wantCPUQuotaUs  int
+	wantCFSPeriod   int
+	wantCPUWeight   int
+	wantCPUMax      string
+}
+
+func TestSetup_ParentCgroups(t *testing.T) {
+	tests := []struct {
+		name       string
+		cfg        cgroups.Config
+		expectedV1 expectedCgroup
+		expectedV2 expectedCgroup
+	}{
+		{
+			name: "all config specified",
+			cfg: cgroups.Config{
+				MemoryBytes: 102400,
+				CPUShares:   256,
+				CPUQuotaUs:  200,
+			},
+			expectedV1: expectedCgroup{
+				wantMemoryBytes: 102400,
+				wantCPUShares:   256,
+				wantCPUQuotaUs:  200,
+				wantCFSPeriod:   int(cfsPeriodUs),
+			},
+			expectedV2: expectedCgroup{
+				wantMemoryBytes: 102400,
+				wantCPUWeight:   256,
+				wantCPUMax:      "200 100000",
+			},
+		},
+		{
+			name: "only memory limit set",
+			cfg: cgroups.Config{
+				MemoryBytes: 102400,
+			},
+			expectedV1: expectedCgroup{
+				wantMemoryBytes: 102400,
+			},
+			expectedV2: expectedCgroup{
+				wantMemoryBytes: 102400,
+			},
+		},
+		{
+			name: "only cpu shares set",
+			cfg: cgroups.Config{
+				CPUShares: 512,
+			},
+			expectedV1: expectedCgroup{
+				wantCPUShares: 512,
+			},
+			expectedV2: expectedCgroup{
+				wantCPUWeight: 512,
+			},
+		},
+		{
+			name: "only cpu quota set",
+			cfg: cgroups.Config{
+				CPUQuotaUs: 200,
+			},
+			expectedV1: expectedCgroup{
+				wantCPUQuotaUs: 200,
+				wantCFSPeriod:  int(cfsPeriodUs),
+			},
+			expectedV2: expectedCgroup{
+				wantCPUMax: "200 100000",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			for _, version := range []int{1, 2} {
+				version := version
+				t.Run("cgroups-v"+strconv.Itoa(version), func(t *testing.T) {
+					t.Parallel()
+
+					mock := newMock(t, version)
+
+					pid := 1
+
+					cfg := tt.cfg
+					cfg.HierarchyRoot = "gitaly"
+					cfg.Mountpoint = mock.rootPath()
+
+					manager := mock.newCgroupManager(cfg, testhelper.SharedLogger(t), pid)
+					mock.setupMockCgroupFiles(t, manager, []uint{})
+
+					require.False(t, manager.Ready())
+					require.NoError(t, manager.Setup())
+					require.True(t, manager.Ready())
+
+					cgroupPath := filepath.Join("gitaly", fmt.Sprintf("gitaly-%d", pid))
+					if version == 1 {
+						requireCgroupComponents(t, version, mock.rootPath(), cgroupPath, tt.expectedV1)
+					} else {
+						requireCgroupComponents(t, version, mock.rootPath(), cgroupPath, tt.expectedV2)
+					}
+				})
+			}
+		})
+	}
+}
+
+func requireCgroupComponents(t *testing.T, version int, root string, cgroupPath string, expected expectedCgroup) {
+	t.Helper()
+
+	if version == 1 {
+		memoryLimitPath := filepath.Join(root, "memory", cgroupPath, "memory.limit_in_bytes")
+		requireCgroupWithInt(t, memoryLimitPath, expected.wantMemoryBytes)
+
+		cpuSharesPath := filepath.Join(root, "cpu", cgroupPath, "cpu.shares")
+		requireCgroupWithInt(t, cpuSharesPath, expected.wantCPUShares)
+
+		cpuCFSQuotaPath := filepath.Join(root, "cpu", cgroupPath, "cpu.cfs_quota_us")
+		requireCgroupWithInt(t, cpuCFSQuotaPath, expected.wantCPUQuotaUs)
+
+		cpuCFSPeriodPath := filepath.Join(root, "cpu", cgroupPath, "cpu.cfs_period_us")
+		requireCgroupWithInt(t, cpuCFSPeriodPath, expected.wantCFSPeriod)
+	} else {
+		memoryMaxPath := filepath.Join(root, cgroupPath, "memory.max")
+		requireCgroupWithInt(t, memoryMaxPath, expected.wantMemoryBytes)
+
+		cpuWeightPath := filepath.Join(root, cgroupPath, "cpu.weight")
+		requireCgroupWithInt(t, cpuWeightPath, calculateWantCPUWeight(expected.wantCPUWeight))
+
+		cpuMaxPath := filepath.Join(root, cgroupPath, "cpu.max")
+		requireCgroupWithString(t, cpuMaxPath, expected.wantCPUMax)
+	}
 }
