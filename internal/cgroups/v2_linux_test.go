e44ef330fa129bc242e3bb3ed34102276eed6f15
Will Chandler <wchandler@gitlab.com>
2023-11-08 22:56:26 -0500

    cgroups: Unify TestStats
    Combine the v1 and v2 versions of `TestStats()` to a single test.
    
    Here we have the opposite of `TestPruneOldCgroups()`, the test cases
    are unique to v1 or v2, but the assertions are version agnostic. Add a
    `version` field to the test cases, rather than iterating over each case
    with each version as we did with the other tests.

--- internal/cgroups/v2_linux_test.go
+++ internal/cgroups/v2_linux_test.go
@@ -10,7 +10,6 @@ import (
 
 	"github.com/stretchr/testify/require"
 	"gitlab.com/gitlab-org/gitaly/v16/internal/gitaly/config/cgroups"
-	"gitlab.com/gitlab-org/gitaly/v16/internal/testhelper"
 	"golang.org/x/exp/slices"
 )
 
@@ -23,83 +22,6 @@ func defaultCgroupsV2Config() cgroups.Config {
 			CPUShares:   256,
 			CPUQuotaUs:  2000,
 		},
-	}
-}
-
-func TestStatsV2(t *testing.T) {
-	t.Parallel()
-
-	for _, tc := range []struct {
-		desc          string
-		mockFiles     []mockCgroupFile
-		expectedStats Stats
-	}{
-		{
-			desc: "empty statistics",
-			mockFiles: []mockCgroupFile{
-				{"memory.current", "0"},
-				{"memory.max", "0"},
-				{"cpu.stat", ""},
-			},
-			expectedStats: Stats{},
-		},
-		{
-			desc: "cgroupfs recorded some stats",
-			mockFiles: []mockCgroupFile{
-				{"memory.max", "2000000000"},
-				{"memory.current", "1234000000"},
-				{"memory.events", `low 1
-high 2
-max 3
-oom 4
-oom_kill 5`},
-				{"nr_throttled", "50"},
-				{"throttled_usec", "1000000"},
-				{"cpu.stat", `nr_periods 10
-nr_throttled 50
-throttled_usec 1000000`}, // 0.001 seconds
-				{"memory.stat", `anon 234000000
-file 235000000
-inactive_anon 200000000
-active_anon 34000000
-inactive_file 100000000
-active_file 135000000`},
-			},
-			expectedStats: Stats{
-				ParentStats: CgroupStats{
-					CPUThrottledCount:    50,
-					CPUThrottledDuration: 0.001,
-					MemoryUsage:          1234000000,
-					MemoryLimit:          2000000000,
-					OOMKills:             5,
-					Anon:                 234000000,
-					ActiveAnon:           34000000,
-					InactiveAnon:         200000000,
-					File:                 235000000,
-					ActiveFile:           135000000,
-					InactiveFile:         100000000,
-				},
-			},
-		},
-	} {
-		t.Run(tc.desc, func(t *testing.T) {
-			mock := newMockV2(t)
-
-			config := defaultCgroupsConfig()
-			config.Repositories.Count = 1
-			config.Repositories.MemoryBytes = 2000000000
-			config.Repositories.CPUShares = 16
-			config.Mountpoint = mock.root
-
-			v2Manager := mock.newCgroupManager(config, testhelper.SharedLogger(t), 1)
-
-			mock.setupMockCgroupFiles(t, v2Manager, []uint{0}, tc.mockFiles...)
-			require.NoError(t, v2Manager.Setup())
-
-			stats, err := v2Manager.Stats()
-			require.NoError(t, err)
-			require.Equal(t, tc.expectedStats, stats)
-		})
 	}
 }
 
